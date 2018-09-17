7fdebcfb0982be2c6cee3a54c9edeec2dfb1f91b
jhpoelen <jhpoelen@xs4all.nl>
2018-09-17 14:29:35 -0700

    add jar magic

--- .travis.deploy_if_tag.sh
+++ .travis.deploy_if_tag.sh
@@ -2,5 +2,6 @@
 if [ -n "$TRAVIS_TAG" ]; then
   mvn -s .travis.maven.settings.xml -DskipTests clean deploy
   VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cp target/preston-${VERSION}-jar-with-dependencies.jar preston.jar
+  cp target/preston-${VERSION}-jar-with-dependencies.jar preston_no_head.jar
+  cat .travis.jar.magic preston_no_head.jar > preston.jar
 fi
