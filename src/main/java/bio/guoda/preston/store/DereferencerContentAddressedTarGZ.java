a1ed84137408c866656028148babda770b1d6278
jhpoelen <jhpoelen@xs4all.nl>
2019-06-25 15:13:38 -0700

    another pass at tar.gz support

--- src/main/java/bio/guoda/preston/store/DereferencerContentAddressedTarGZ.java
+++ src/main/java/bio/guoda/preston/store/DereferencerContentAddressedTarGZ.java
@@ -10,49 +10,56 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 
-public class DereferencerContentAddressedTarGZ implements Dereferencer<IRI> {
+public class DereferencerContentAddressedTarGZ implements Dereferencer<InputStream> {
     private final Dereferencer<InputStream> dereferencer;
-    private final BlobStore blobStore;
 
-    public DereferencerContentAddressedTarGZ(Dereferencer<InputStream> dereferencer, BlobStore blobStore) {
+    public DereferencerContentAddressedTarGZ(Dereferencer<InputStream> dereferencer) {
         this.dereferencer = dereferencer;
-        this.blobStore = blobStore;
     }
 
     @Override
-    public IRI dereference(IRI uri) throws IOException {
+    public InputStream dereference(IRI uri) throws IOException {
         String iriString = uri == null ? null : uri.getIRIString();
-        IRI dereferenced = null;
+        InputStream inputStream = null;
         if (StringUtils.startsWith(iriString, "tgz:")) {
-            String[] tarUrlSplit = iriString.substring(4, iriString.length()).split("!/");
+            String iriSuffix = StringUtils.substring(iriString, 4);
+            String[] tarUrlSplit = iriSuffix == null ? new String[]{} : iriSuffix.split("!/");
             if (tarUrlSplit.length == 2) {
                 String archiveURL = tarUrlSplit[0];
                 String hashPath = tarUrlSplit[1];
-                String[] hashPathElem = hashPath.split("/");
-                String lastHashElem = hashPathElem[hashPathElem.length - 1];
-                String expectedHashKey = "hash://sha256/" + lastHashElem;
-                if (HashKeyUtil.isValidHashKey(expectedHashKey)) {
-                    try (InputStream data = dereferencer == null
+                IRI expectedHashIRI = extractHashURI(hashPath);
+                if (expectedHashIRI != null) {
+                    InputStream data = dereferencer == null
                             ? null
-                            : dereferencer.dereference(RefNodeFactory.toIRI(archiveURL))) {
-                        if (data != null) {
-                            IRI expectedHashIRI = RefNodeFactory.toIRI(expectedHashKey);
-                            TarArchiveInputStream tarInputStream = new TarArchiveInputStream(new GZIPInputStream(data));
-                            TarArchiveEntry entry = null;
-                            while ((entry = tarInputStream.getNextTarEntry()) != null) {
-                                if (entry.isFile()) {
-                                    IRI iri = blobStore.putBlob(tarInputStream);
-                                    if (iri != null && iri.equals(expectedHashIRI)) {
-                                        dereferenced = expectedHashIRI;
-                                    }
+                            : dereferencer.dereference(RefNodeFactory.toIRI(archiveURL));
+                    if (data != null) {
+                        TarArchiveInputStream tarInputStream = new TarArchiveInputStream(new GZIPInputStream(data));
+                        TarArchiveEntry entry;
+                        while ((entry = tarInputStream.getNextTarEntry()) != null) {
+                            if (entry.isFile()) {
+                                IRI foundHashIRI = extractHashURI(entry.getName());
+                                if (foundHashIRI != null && foundHashIRI.equals(expectedHashIRI)) {
+                                    inputStream = tarInputStream;
+                                    break;
                                 }
                             }
                         }
                     }
+
                 }
             }
         }
-        return dereferenced;
+        return inputStream;
+    }
+
+    public IRI extractHashURI(String hashPath) {
+        String[] hashPathElem = hashPath.split("/");
+        String lastHashElem = hashPathElem[hashPathElem.length - 1];
+
+        String hashString = "hash://sha256/" + lastHashElem;
+        return HashKeyUtil.isValidHashKey(hashString)
+                ? RefNodeFactory.toIRI(hashString)
+                : null;
     }
 
 }
--- src/test/java/bio/guoda/preston/store/DereferencerContentAddressedTarGZTest.java
+++ src/test/java/bio/guoda/preston/store/DereferencerContentAddressedTarGZTest.java
@@ -1,6 +1,8 @@
 package bio.guoda.preston.store;
 
+import bio.guoda.preston.Hasher;
 import bio.guoda.preston.model.RefNodeFactory;
+import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.rdf.api.IRI;
 import org.hamcrest.core.Is;
 import org.junit.Test;
@@ -9,32 +11,42 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class DereferencerContentAddressedTarGZTest {
 
     @Test
-    public void pullThroughArchive() throws IOException {
+    public void pickHashFromTarball() throws IOException {
 
-        KeyValueStore testPersistence = TestUtil.getTestPersistence();
-        BlobStore blobStore = new BlobStoreAppendOnly(testPersistence, false);
+        String knownPresentHash = "hash://sha256/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99";
+        assertThat(knownPresentHash.length(), Is.is(78));
 
-        DereferencerContentAddressedTarGZ derefTarGZ = new DereferencerContentAddressedTarGZ(new Dereferencer<InputStream>() {
+        try (InputStream is = getDerefTarGZ().dereference(RefNodeFactory.toIRI("tgz:https://example.com/preston-1a.tar.gz!/1a/57/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99"))) {
+            assertNotNull(is);
+            IRI iri = Hasher.calcSHA256(is, new NullOutputStream(), false);
+            assertThat(iri, Is.is(RefNodeFactory.toIRI(knownPresentHash)));
+        }
+
+    }
+
+    @Test
+    public void missingHashExistingTar() throws IOException {
+        assertNull(getDerefTarGZ().dereference(RefNodeFactory.toIRI("tgz:https://example.com/preston-1a.tar.gz!/1a/57/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a00")));
+    }
+
+    @Test
+    public void existingHashMissingTar() throws IOException {
+        assertNull(getDerefTarGZ().dereference(RefNodeFactory.toIRI("tgz:https://example.com/preston-2a.tar.gz!/1a/57/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a00")));
+    }
+
+    public DereferencerContentAddressedTarGZ getDerefTarGZ() {
+        return new DereferencerContentAddressedTarGZ(new Dereferencer<InputStream>() {
             @Override
             public InputStream dereference(IRI uri) throws IOException {
-                return getClass().getResourceAsStream("/preston-a1.tar.gz");
+                return getClass().getResourceAsStream("/preston-1a.tar.gz");
             }
-        }, blobStore);
-
-        String hashKey = "hash://sha256/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99";
-        assertThat(hashKey.length(), Is.is(78));
-
-        IRI dereference = derefTarGZ.dereference(RefNodeFactory.toIRI("tgz:https://example.com/preston-a1.tar.gz!/1a/57/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99"));
-
-        assertThat(dereference, Is.is(RefNodeFactory.toIRI("hash://sha256/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99")));
-
-        assertNotNull(testPersistence.get("hash://sha256/a12dd6335e7803027da3007e26926c5c946fea9803a5eb07908d978998d933da"));
-        assertNotNull(testPersistence.get("hash://sha256/1a57e55a780b86cff38697cf1b857751ab7b389973d35113564fe5a9a58d6a99"));
+        });
     }
 
 }
