cddd3ea0772b82884d75a8742571377423c3297f
jhpoelen <jhpoelen@xs4all.nl>
2019-06-27 12:49:42 -0700

    first pass at fixing #13

--- src/main/java/bio/guoda/preston/cmd/CmdGet.java
+++ src/main/java/bio/guoda/preston/cmd/CmdGet.java
@@ -2,7 +2,7 @@ package bio.guoda.preston.cmd;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.ProxyInputStream;
 import org.apache.commons.lang3.StringUtils;
 import bio.guoda.preston.model.RefNodeFactory;
 import bio.guoda.preston.store.BlobStoreAppendOnly;
@@ -11,10 +11,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.System.exit;
+import static java.lang.System.in;
+import static org.apache.commons.io.IOUtils.EOF;
 
 @Parameters(separators = "= ", commandDescription = "get biodiversity node(s)")
 public class CmdGet extends Persisting implements Runnable {
@@ -54,10 +58,18 @@ public class CmdGet extends Persisting implements Runnable {
                 System.err.println("not found: [" + hash + "]");
                 exit(1);
             }
-            IOUtils.copy(input, System.out);
+            copyIfNoError(input, System.out);
 
         } catch (IOException e) {
             throw new IOException("problem retrieving [" + hash + "]", e);
+        }
+    }
+
+    public static void copyIfNoError(InputStream proxyIs, PrintStream out) throws IOException {
+        byte[] buffer = new byte[4096];
+        int n;
+        while (!out.checkError() && EOF != (n = proxyIs.read(buffer))) {
+            System.out.write(buffer, 0, n);
         }
     }
 
