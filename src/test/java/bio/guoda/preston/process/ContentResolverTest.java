680ec8e15fea894f2dd29d2626b6a8521a01fc6f
jhpoelen <jhpoelen@xs4all.nl>
2019-08-19 15:05:55 -0700

    fix test error

--- src/test/java/bio/guoda/preston/process/ContentResolverTest.java
+++ src/test/java/bio/guoda/preston/process/ContentResolverTest.java
@@ -76,7 +76,7 @@ public class ContentResolverTest {
 
         assertTrue(tempDir.toFile().exists());
         assertFalse(refNodes.isEmpty());
-        assertThat(refNodes.size(), is(3));
+        assertThat(refNodes.size(), is(7));
 
         assertThat(refNodes.get(1).getPredicate(), is(RefNodeConstants.WAS_GENERATED_BY));
         assertThat(refNodes.get(1).getObject(), is(TestUtil.getTestCrawlContext().getActivity()));
@@ -84,7 +84,7 @@ public class ContentResolverTest {
         String expectedHash = "50d7a905e3046b88638362cc34a31a1ae534766ca55e3aa397951efe653b062b";
         String expectedValue = "https://example.org";
 
-        Triple cachedNode = refNodes.get(2);
+        Triple cachedNode = refNodes.get(6);
         assertContentWith(cachedNode, expectedHash, expectedValue);
         InputStream inputStream = blobStore.get((IRI) cachedNode.getObject());
         assertNotNull(inputStream);
@@ -98,7 +98,7 @@ public class ContentResolverTest {
         assertTrue(data.exists());
         assertThat(IOUtils.toString(data.toURI(), StandardCharsets.UTF_8), is(expectedValue));
 
-        assertContentWith(refNodes.get(2), expectedHash, expectedValue);
+        assertContentWith(refNodes.get(6), expectedHash, expectedValue);
 
         FileUtils.deleteQuietly(tempDir.toFile());
     }
