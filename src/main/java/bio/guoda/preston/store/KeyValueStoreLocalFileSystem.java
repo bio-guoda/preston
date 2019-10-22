5e218976601570a533ccbb9f50baf00f9453b276
jhpoelen <jhpoelen@xs4all.nl>
2019-10-21 19:21:09 -0700

    add counter and listener for keyvaluestore. Related to #26

--- src/main/java/bio/guoda/preston/store/KeyValueStoreLocalFileSystem.java
+++ src/main/java/bio/guoda/preston/store/KeyValueStoreLocalFileSystem.java
@@ -1,6 +1,7 @@
 package bio.guoda.preston.store;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.input.CountingInputStream;
 import org.apache.commons.rdf.api.IRI;
 
 import java.io.File;
@@ -8,16 +9,35 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 public class KeyValueStoreLocalFileSystem implements KeyValueStore {
 
     private final File tmpDir;
     private final KeyToPath keyToPath;
+    private final KeyValueStoreListener listener;
+    private AtomicLong totalBytes = new AtomicLong(0);
+    private AtomicLong totalKeys = new AtomicLong(0L);
 
     public KeyValueStoreLocalFileSystem(File tmpDir, KeyToPath keyToPath) {
+        this(tmpDir, keyToPath, new KeyValueStoreListener() {
+            @Override
+            public void beforePut(IRI key, long valueSizeInBytes) {
+
+            }
+
+            @Override
+            public void afterPut(IRI key, long valueSizeInBytes) {
+
+            }
+        });
+    }
+
+    public KeyValueStoreLocalFileSystem(File tmpDir, KeyToPath keyToPath, KeyValueStoreListener listener) {
         this.tmpDir = tmpDir;
         this.keyToPath = keyToPath;
+        this.listener = listener;
     }
 
     private static File getDataFile(URI filePath) {
@@ -25,20 +45,36 @@ public class KeyValueStoreLocalFileSystem implements KeyValueStore {
     }
 
 
+    interface KeyValueStoreListener {
+        void beforePut(IRI key, long valueSizeInBytes);
+        void afterPut(IRI key, long valueSizeInBytes);
+    }
+
     @Override
     public void put(IRI key, InputStream source) throws IOException {
         try (InputStream is = source) {
-            URI filePath = keyToPath.toPath(key);
-            if (!getDataFile(filePath).exists()) {
-                File destFile = getDataFile(filePath);
-                File tmpDestFile = getDataFile(URI.create(filePath.toString() + ".tmp"));
+            URI filePathBeforeCopy = keyToPath.toPath(key);
+            if (!getDataFile(filePathBeforeCopy).exists()) {
+                File tmpDestFile = getDataFile(URI.create(filePathBeforeCopy.toString() + ".tmp"));
                 tmpDestFile.deleteOnExit();
-                FileUtils.forceMkdirParent(destFile);
+                File destFile = null;
                 try {
-                    FileUtils.copyToFile(is, tmpDestFile);
-                    if (!tmpDestFile.renameTo(destFile)) {
-                        throw new IOException("failed to rename tmp file from [" + tmpDestFile.getAbsolutePath() + "] to [" + destFile.getAbsolutePath() + "]");
+                    CountingInputStream isCounting = new CountingInputStream(is);
+                    FileUtils.copyToFile(isCounting, tmpDestFile);
+
+                    listener.beforePut(key, isCounting.getByteCount());
+
+                    URI filePathAfterCopy = keyToPath.toPath(key);
+                    destFile = getDataFile(filePathAfterCopy);
+                    if (!destFile.exists()) {
+                        FileUtils.forceMkdirParent(destFile);
+                        if (!tmpDestFile.renameTo(destFile)) {
+                            throw new IOException("failed to rename tmp file from [" + tmpDestFile.getAbsolutePath() + "] to [" + destFile.getAbsolutePath() + "]");
+                        }
                     }
+
+                    listener.afterPut(key, isCounting.getByteCount());
+
                 } catch (IOException ex) {
                     FileUtils.deleteQuietly(tmpDestFile);
                     FileUtils.deleteQuietly(destFile);
--- src/test/java/bio/guoda/preston/store/KeyValueStoreLocalFileSystemTest.java
+++ src/test/java/bio/guoda/preston/store/KeyValueStoreLocalFileSystemTest.java
@@ -11,12 +11,15 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -26,7 +29,9 @@ import static org.junit.Assert.assertThat;
 
 public class KeyValueStoreLocalFileSystemTest {
 
-    public static final IRI SOME_HASH = RefNodeFactory.toIRI("hash://sha256/1234567890123456789012345678901234567890123456789012345678901234");
+    public static final IRI SOME_HASH
+            = RefNodeFactory.toIRI("hash://sha256/1234567890123456789012345678901234567890123456789012345678901234");
+
     private Path path;
 
     @Before
@@ -62,7 +67,41 @@ public class KeyValueStoreLocalFileSystemTest {
 
     @Test
     public void writeStream() throws IOException {
-        KeyValueStoreLocalFileSystem filePersistence = new KeyValueStoreLocalFileSystem(new File(path.toFile(), "tmp"), new KeyTo3LevelPath(new File(path.toFile(), "datasets").toURI()));
+        AtomicLong beforeCount = new AtomicLong(0);
+        AtomicLong afterCount = new AtomicLong(0);
+        List<IRI> pathRequests = new ArrayList<>();
+        AtomicBoolean isAsserting = new AtomicBoolean(true);
+        StringBuffer sequence = new StringBuffer();
+
+        KeyValueStoreLocalFileSystem filePersistence =
+                new KeyValueStoreLocalFileSystem(
+                        new File(path.toFile(), "tmp"),
+                        new KeyToPath() {
+                            private KeyToPath keyToPath = new KeyTo3LevelPath(new File(path.toFile(), "datasets").toURI());
+                            @Override
+                            public URI toPath(IRI key) {
+                                if (!isAsserting.get()) {
+                                    sequence.append("toPath");
+                                    pathRequests.add(key);
+                                }
+                                return keyToPath.toPath(key);
+                            }
+                        }
+        ,
+                        new KeyValueStoreLocalFileSystem.KeyValueStoreListener() {
+
+                            @Override
+                            public void beforePut(IRI key, long valueSizeInBytes) {
+                                beforeCount.addAndGet(valueSizeInBytes);
+                                sequence.append("-beforePut-");
+                            }
+
+                            @Override
+                            public void afterPut(IRI key, long valueSizeInBytes) {
+                                afterCount.addAndGet(valueSizeInBytes);
+                                sequence.append("-afterPut");
+                            }
+                        });
 
         assertThat(filePersistence.get(SOME_HASH), is(nullValue()));
         final InputStream someContentStream = IOUtils.toInputStream("some content", StandardCharsets.UTF_8);
@@ -81,14 +120,28 @@ public class KeyValueStoreLocalFileSystemTest {
         };
 
         assertFalse(wasClosed.get());
+
+        isAsserting.set(false);
+
         filePersistence.put((is, os) -> {
             IOUtils.copy(is, os);
             return SOME_HASH;
         }, wrappedContentStream);
 
+        isAsserting.set(true);
+
         assertFalse(wasClosed.get());
 
         assertThat(TestUtil.toUTF8(filePersistence.get(SOME_HASH)), is("some content"));
+
+        long valueLength = (long) IOUtils.toByteArray(filePersistence.get(SOME_HASH)).length;
+        assertThat(afterCount.get(), is(valueLength));
+        assertThat(beforeCount.get(), is(12L));
+
+        assertThat(pathRequests.size(), is(2));
+        assertThat(pathRequests.get(0), is(SOME_HASH));
+        assertThat(pathRequests.get(1), is(SOME_HASH));
+        assertThat(sequence.toString(), is("toPath-beforePut-toPath-afterPut"));
 
 
     }
