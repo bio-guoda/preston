281fcd93c407968137adbf9df24d3d6d03d49c69
jhpoelen <jhpoelen@xs4all.nl>
2019-11-04 15:51:12 -0800

    add streaming sha256 hash uri validator; related to #31

---
+++ src/main/java/bio/guoda/preston/store/ValidatingKeyValueStreamSHA256IRI.java
@@ -0,0 +1,38 @@
+package bio.guoda.preston.store;
+
+import org.apache.commons.io.input.BoundedInputStream;
+import org.apache.commons.io.input.TeeInputStream;
+import org.apache.commons.rdf.api.IRI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+
+public class ValidatingKeyValueStreamSHA256IRI implements ValidatingKeyValueStream {
+
+    private final static Pattern SHA_256_PATTERN = Pattern.compile("hash://sha256/([a-z0-9]){64}");
+
+    private final IRI key;
+    private final ByteArrayOutputStream baos;
+    private final InputStream value;
+
+    public ValidatingKeyValueStreamSHA256IRI(IRI key, InputStream value) {
+        this.key = key;
+        this.baos = new ByteArrayOutputStream();
+        this.value = new TeeInputStream(new BoundedInputStream(value, 79), baos);
+    }
+
+    @Override
+    public InputStream getValueStream() {
+        return value;
+    }
+
+    @Override
+    public boolean acceptValueStreamForKey(IRI key) {
+        byte[] bytes = baos.toByteArray();
+        String sha256Hash = new String(bytes, StandardCharsets.UTF_8);
+        return bytes.length == 78
+                && SHA_256_PATTERN.matcher(sha256Hash).matches();
+    }
+}
