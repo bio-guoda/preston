2baf81cd12bc76f73f236203d450e95264fdc1fe
jhpoelen <jhpoelen@xs4all.nl>
2020-02-12 15:23:45 -0800

    remove linux pipe signal handle provided in non-standard api

--- src/main/java/bio/guoda/preston/cmd/Cmd.java
+++ src/main/java/bio/guoda/preston/cmd/Cmd.java
@@ -1,24 +1,22 @@
 package bio.guoda.preston.cmd;
 
-import sun.misc.Signal;
-
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Cmd implements ProcessorState {
 
     private static AtomicBoolean shouldKeepProcessing = new AtomicBoolean(true);
 
-    static {
-        try {
-            Signal signal = new Signal("PIPE");
-            Signal.handle(signal, signal1 -> {
-                shouldKeepProcessing.set(false);
-            });
-        } catch (IllegalArgumentException ex) {
-            // ignore, probably not linux/posix system (e.g., windows)
-        }
-
-    }
+//    static {
+//        try {
+//            Signal signal = new Signal("PIPE");
+//            Signal.handle(signal, signal1 -> {
+//                shouldKeepProcessing.set(false);
+//            });
+//        } catch (IllegalArgumentException ex) {
+//            // ignore, probably not linux/posix system (e.g., windows)
+//        }
+//
+//    }
 
     @Override
     public boolean shouldKeepProcessing() {
