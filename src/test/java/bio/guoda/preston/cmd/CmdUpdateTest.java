c8f2a364cadf787e08a69053919a1770df96f035
jhpoelen <jhpoelen@xs4all.nl>
2020-03-11 12:21:22 -0700

    add test case

--- src/test/java/bio/guoda/preston/cmd/CmdUpdateTest.java
+++ src/test/java/bio/guoda/preston/cmd/CmdUpdateTest.java
@@ -5,25 +5,35 @@ import bio.guoda.preston.store.StatementStore;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.RDFTerm;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertThat;
 
 public class CmdUpdateTest {
 
+    private static final AtomicInteger putAttemptCount = new AtomicInteger(0);
+
     @Test
     public void doUpdate() {
+        assertThat(putAttemptCount.get(), Is.is(0));
         new CmdUpdate().run(
                 new BlobStoreNull(),
                 new StatementStoreNull());
-
-
+        assertThat(putAttemptCount.get() > 0, Is.is(true));
     }
 
     private static class BlobStoreNull implements BlobStore {
+
+
         @Override
         public IRI put(InputStream is) throws IOException {
+            putAttemptCount.incrementAndGet();
             return null;
         }
 
