cf973e5fc221d7c866bc3c3ed65c6043840e67b8
jhpoelen <jhpoelen@xs4all.nl>
2020-03-11 13:44:57 -0700

    add RDFUtil test; related to #41

---
+++ src/test/java/bio/guoda/preston/RDFUtilTest.java
@@ -0,0 +1,30 @@
+package bio.guoda.preston;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.sparql.core.Quad;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class RDFUtilTest {
+
+    @Test
+    public void asQuadStream() {
+        Stream<org.apache.commons.rdf.api.Quad> quadStream = RDFUtil.asQuadStream(IOUtils.toInputStream("<subj> <verb> <obj> <graph> .", StandardCharsets.UTF_8));
+        List<org.apache.commons.rdf.api.Quad> collect = quadStream.collect(Collectors.toList());
+        assertThat(collect.size(), is(1));
+        org.apache.commons.rdf.api.Quad quad = collect.get(0);
+        assertThat(quad.getObject().toString(), is("obj"));
+        assertThat(quad.getPredicate().toString(), is("verb"));
+        assertThat(quad.getSubject().toString(), is("subj"));
+        assertThat(quad.getGraphName().isPresent(), is(true));
+        assertThat(quad.getGraphName().get(), is("graph"));
+    }
+
+}
