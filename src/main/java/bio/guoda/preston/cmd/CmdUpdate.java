bc5aeeb63f5175542f4dee6f7063a862639e68cd
Michael Elliott <mielliott@ufl.edu>
2020-03-11 15:56:11 -0400

    add graph name to seed statements. Related to #41

--- src/main/java/bio/guoda/preston/cmd/CmdUpdate.java
+++ src/main/java/bio/guoda/preston/cmd/CmdUpdate.java
@@ -1,29 +1,26 @@
 package bio.guoda.preston.cmd;
 
-import bio.guoda.preston.Resources;
-import bio.guoda.preston.Seeds;
-import bio.guoda.preston.process.ActivityTracker;
-import bio.guoda.preston.process.StatementListener;
-import bio.guoda.preston.store.Archiver;
-import bio.guoda.preston.store.BlobStore;
-import bio.guoda.preston.store.DereferencerContentAddressed;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.Parameters;
-import org.apache.commons.rdf.api.IRI;
-import org.apache.commons.rdf.api.Triple;
-import org.apache.commons.rdf.api.Quad;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import static bio.guoda.preston.RefNodeConstants.HAS_VERSION;
 import static bio.guoda.preston.RefNodeConstants.WAS_ASSOCIATED_WITH;
 import static bio.guoda.preston.model.RefNodeFactory.toBlank;
 import static bio.guoda.preston.model.RefNodeFactory.toIRI;
 import static bio.guoda.preston.model.RefNodeFactory.toStatement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+
+import bio.guoda.preston.Seeds;
+import bio.guoda.preston.process.ActivityTracker;
+import bio.guoda.preston.process.StatementListener;
+import bio.guoda.preston.store.BlobStore;
 
 @Parameters(separators = "= ", commandDescription = "update biodiversity dataset graph")
 public class CmdUpdate extends CmdActivity {
@@ -71,7 +68,7 @@ public class CmdUpdate extends CmdActivity {
 
     private List<Quad> generateSeeds(final IRI crawlActivity) {
         return seedUrls.stream()
-                .map((String uriString) -> toStatement(toIRI(uriString), WAS_ASSOCIATED_WITH, crawlActivity))
+                .map((String uriString) -> toStatement(crawlActivity, toIRI(uriString), WAS_ASSOCIATED_WITH, crawlActivity))
                 .collect(Collectors.toList());
     }
 
