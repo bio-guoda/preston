b2b7d0fa45a396e2ab66d750ef605581d96dc6e2
Michael <mielliott@ufl.edu>
2020-03-21 13:42:09 -0400

    Changed activity functions to create activity UUIDs and IS_A, WAS_INFORMED_BY statements

--- src/test/java/bio/guoda/preston/process/ActivityUtilTest.java
+++ src/test/java/bio/guoda/preston/process/ActivityUtilTest.java
@@ -7,63 +7,54 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import static bio.guoda.preston.RefNodeConstants.ACTIVITY;
 import static bio.guoda.preston.RefNodeConstants.IS_A;
+import static bio.guoda.preston.RefNodeConstants.WAS_INFORMED_BY;
 import static bio.guoda.preston.model.RefNodeFactory.toIRI;
 import static bio.guoda.preston.model.RefNodeFactory.toStatement;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ActivityUtilTest {
 
     @Test
-    public void startActivity() {
-        List<Quad> listQuads = new ArrayList<>();
-        Optional<BlankNodeOrIRI> sourceActivity = Optional.of(toIRI("activityIRI"));
-
-        BlankNodeOrIRI newActivity = ActivityUtil.beginInformedActivity(listQuads::add, sourceActivity);
-
-        assertThat(newActivity, is(toIRI("activityIRI")));
-//        assertThat(listQuads.size(), greaterThan(0));
-//        assertThat(listQuads.get(0), is(toStatement(toIRI("activityIRI"), IS_A, ACTIVITY)));
-    }
-
-    @Test
-    public void endActivity() {
-        List<Quad> listQuads = new ArrayList<>();
-        BlankNodeOrIRI activity = toIRI("someActivity");
-
-        ActivityUtil.endInformedActivity(listQuads::add, activity);
-
-        assertThat(listQuads.size(), is(0));
-    }
-
-    @Test
-    public void emitActivityStatement() {
-        List<Quad> listQuads = new ArrayList<>();
-        BlankNodeOrIRI activity = toIRI("activityIRI");
-        Quad unlabeledStatement = toStatement(toIRI("cats"), toIRI("are"), toIRI("small"));
-
-        ActivityUtil.emitWithActivityName(Stream.of(unlabeledStatement), listQuads::add, activity);
-
-        assertThat(listQuads.size(), is(1));
-        assertThat(listQuads.get(0), is(toStatement(activity, unlabeledStatement.getSubject(), unlabeledStatement.getPredicate(), unlabeledStatement.getObject())));
-    }
-
-    @Test
-    public void emitActivity() {
-        List<Quad> listQuads = new ArrayList<>();
-        BlankNodeOrIRI parentActivity = toIRI("someActivity");
+    public void emitUninformedActivity() {
+        List<Quad> nodes = new ArrayList<>();
         Quad first = toStatement(toIRI("cats"), toIRI("are"), toIRI("small"));
         Stream<Quad> unlabeledStatements = Stream.of(first);
 
-        BlankNodeOrIRI newActivity = ActivityUtil.emitAsNewActivity(unlabeledStatements, listQuads::add, Optional.of(parentActivity));
+        final BlankNodeOrIRI newActivity = ActivityUtil.emitAsNewActivity(unlabeledStatements, nodes::add, Optional.empty());
 
-        assertThat(listQuads.size(), greaterThan(0));
-        assertTrue(listQuads.contains(toStatement(newActivity, first.getSubject(), first.getPredicate(), first.getObject())));
+        assertTrue(isUUID(newActivity));
+
+        assertThat(nodes.size(), is(2));
+        assertTrue(nodes.contains(toStatement(newActivity, newActivity, IS_A, ACTIVITY)));
+        assertTrue(nodes.contains(toStatement(newActivity, first.getSubject(), first.getPredicate(), first.getObject())));
+    }
+
+    @Test
+    public void emitInformedActivity() {
+        List<Quad> nodes = new ArrayList<>();
+        final BlankNodeOrIRI sourceActivity = toIRI("someActivity");
+        Quad first = toStatement(toIRI("cats"), toIRI("are"), toIRI("small"));
+        Stream<Quad> unlabeledStatements = Stream.of(first);
+
+        final BlankNodeOrIRI newActivity = ActivityUtil.emitAsNewActivity(unlabeledStatements, nodes::add, Optional.of(sourceActivity));
+
+        assertTrue(isUUID(newActivity));
+
+        assertThat(nodes.size(), is(3));
+        assertTrue(nodes.contains(toStatement(newActivity, newActivity, IS_A, ACTIVITY)));
+        assertTrue(nodes.contains(toStatement(newActivity, newActivity, WAS_INFORMED_BY, sourceActivity)));
+        assertTrue(nodes.contains(toStatement(newActivity, first.getSubject(), first.getPredicate(), first.getObject())));
+    }
+
+    private boolean isUUID(BlankNodeOrIRI uuid) {
+        String uuidString = uuid.toString();
+        return uuidString.matches("^<[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}>$");
     }
 }
