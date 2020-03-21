060fdab01dbb13d20db1639fe8aa54f0d1f4c866
Michael <mielliott@ufl.edu>
2020-03-21 13:43:20 -0400

    Changed activity functions to create activity UUIDs and IS_A, WAS_INFORMED_BY statements

--- src/main/java/bio/guoda/preston/process/ActivityUtil.java
+++ src/main/java/bio/guoda/preston/process/ActivityUtil.java
@@ -1,41 +1,45 @@
 package bio.guoda.preston.process;
 
+import bio.guoda.preston.model.RefNodeFactory;
 import org.apache.commons.rdf.api.BlankNodeOrIRI;
-import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.Quad;
-import org.apache.jena.shared.uuid.JenaUUID;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import static bio.guoda.preston.RefNodeConstants.ACTIVITY;
+import static bio.guoda.preston.RefNodeConstants.ENDED_AT_TIME;
+import static bio.guoda.preston.RefNodeConstants.IS_A;
+import static bio.guoda.preston.RefNodeConstants.STARTED_AT_TIME;
+import static bio.guoda.preston.RefNodeConstants.WAS_INFORMED_BY;
 import static bio.guoda.preston.model.RefNodeFactory.toIRI;
 import static bio.guoda.preston.model.RefNodeFactory.toStatement;
 
 public class ActivityUtil {
 
-    private static BlankNodeOrIRI beginInformedActivity(StatementEmitter emitter, Optional<BlankNodeOrIRI> parentActivity) {
-        List<Quad> statements = new LinkedList<>();
-        BlankNodeOrIRI newActivity = parentActivity.orElse(null);
+    private static final boolean emitStartedAt = false;
+    private static final boolean emitEndedAt = false;
 
-//        statements.add(toStatement(newActivity, newActivity, IS_A, ACTIVITY));
+    private static BlankNodeOrIRI beginInformedActivity(StatementEmitter emitter, Optional<BlankNodeOrIRI> sourceActivity) {
+        BlankNodeOrIRI newActivity = toIRI(UUID.randomUUID());
+        emitter.emit(toStatement(newActivity, newActivity, IS_A, ACTIVITY));
 
-//        if (sourceActivity.isPresent()) {
-//            statements.add(
-//                    toStatement(newActivity, newActivity, WAS_INFORMED_BY, sourceActivity.get())
-//            );
-//        }
+        if (sourceActivity.isPresent()) {
+            emitter.emit(toStatement(newActivity, newActivity, WAS_INFORMED_BY, sourceActivity.get()));
+        }
 
-//        statements.add(toStatement(newActivity, newActivity, STARTED_AT, time));
-        statements.forEach(emitter::emit);
+        if (emitStartedAt) {
+            emitter.emit(toStatement(newActivity, newActivity, STARTED_AT_TIME, RefNodeFactory.nowDateTimeLiteral()));
+        }
 
         return newActivity;
     }
 
     private static void endInformedActivity(StatementEmitter emitter, BlankNodeOrIRI activity) {
-//        emitter.emit(toStatement(activity, activity, toIRI("http://www.w3.org/ns/prov#endedAtTime"), RefNodeFactory.nowDateTimeLiteral()));
+        if (emitEndedAt) {
+            emitter.emit(toStatement(activity, activity, ENDED_AT_TIME, RefNodeFactory.nowDateTimeLiteral()));
+        }
     }
 
     private static void emitWithActivityName(Stream<Quad> quadStream, StatementEmitter emitter, BlankNodeOrIRI activity) {
