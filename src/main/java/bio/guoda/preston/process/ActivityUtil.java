ab4dedd441707173a9f60e12e54d5aa06f8a6972
Michael <mielliott@ufl.edu>
2020-03-24 18:11:15 -0400

    Remove unnecessary constants and code bits

--- src/main/java/bio/guoda/preston/process/ActivityUtil.java
+++ src/main/java/bio/guoda/preston/process/ActivityUtil.java
@@ -1,6 +1,5 @@
 package bio.guoda.preston.process;
 
-import bio.guoda.preston.model.RefNodeFactory;
 import org.apache.commons.rdf.api.BlankNodeOrIRI;
 import org.apache.commons.rdf.api.Quad;
 
@@ -9,17 +8,12 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 import static bio.guoda.preston.RefNodeConstants.ACTIVITY;
-import static bio.guoda.preston.RefNodeConstants.ENDED_AT_TIME;
 import static bio.guoda.preston.RefNodeConstants.IS_A;
-import static bio.guoda.preston.RefNodeConstants.STARTED_AT_TIME;
 import static bio.guoda.preston.RefNodeConstants.WAS_INFORMED_BY;
 import static bio.guoda.preston.model.RefNodeFactory.toIRI;
 import static bio.guoda.preston.model.RefNodeFactory.toStatement;
 
 public class ActivityUtil {
-
-    private static final boolean emitStartedAt = false;
-    private static final boolean emitEndedAt = false;
 
     private static void beginInformedActivity(StatementEmitter emitter, BlankNodeOrIRI newActivity, Optional<BlankNodeOrIRI> sourceActivity) {
         emitter.emit(toStatement(newActivity, newActivity, IS_A, ACTIVITY));
@@ -27,16 +21,9 @@ public class ActivityUtil {
         if (sourceActivity.isPresent()) {
             emitter.emit(toStatement(newActivity, newActivity, WAS_INFORMED_BY, sourceActivity.get()));
         }
-
-        if (emitStartedAt) {
-            emitter.emit(toStatement(newActivity, newActivity, STARTED_AT_TIME, RefNodeFactory.nowDateTimeLiteral()));
-        }
     }
 
     private static void endInformedActivity(StatementEmitter emitter, BlankNodeOrIRI activity) {
-        if (emitEndedAt) {
-            emitter.emit(toStatement(activity, activity, ENDED_AT_TIME, RefNodeFactory.nowDateTimeLiteral()));
-        }
     }
 
     private static void emitWithActivityName(Stream<Quad> quadStream, StatementEmitter emitter, BlankNodeOrIRI activity) {
