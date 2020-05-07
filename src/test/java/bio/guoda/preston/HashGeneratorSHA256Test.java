32f5c23163ca45b80308283283f1ec75eeedb2be
jhpoelen <jhpoelen@xs4all.nl>
2020-05-07 10:08:20 -0700

    revert ineffective mvn encoding config; add another encoding test

--- src/test/java/bio/guoda/preston/HashGeneratorSHA256Test.java
+++ src/test/java/bio/guoda/preston/HashGeneratorSHA256Test.java
@@ -1,9 +1,12 @@
 package bio.guoda.preston;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.rdf.api.IRI;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -11,9 +14,16 @@ import static org.junit.Assert.*;
 public class HashGeneratorSHA256Test {
 
     @Test
-    public void hashText() throws IOException {
-        IRI hash = new HashGeneratorSHA256().hash(getClass().getResourceAsStream("/bio/guoda/preston/process/idigbio-recordsets-complete.json"));
+    public void hashTextFromResource() throws IOException {
+        InputStream is = getClass().getResourceAsStream("/bio/guoda/preston/process/idigbio-recordsets-complete.json");
+        IRI hash = new HashGeneratorSHA256().hash(is);
         assertThat(hash.getIRIString(), is("hash://sha256/0931a831be557bfedd27b0068d9a0a9f14c1b92cbf9199e8ba79e04d0a6baedc"));
+    };
+
+    @Test
+    public void hashTextFromString() throws IOException {
+        IRI hash = new HashGeneratorSHA256().hash(IOUtils.toInputStream("hello", StandardCharsets.UTF_8));
+        assertThat(hash.getIRIString(), is("hash://sha256/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"));
     };
 
     @Test
