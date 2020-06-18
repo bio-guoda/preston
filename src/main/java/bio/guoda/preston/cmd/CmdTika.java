fa7f6ce7db721589827f438f2ef4d7efaf2e7c58
jhpoelen <jhpoelen@xs4all.nl>
2020-06-18 09:27:46 -0700

    add description of Tika activity

--- src/main/java/bio/guoda/preston/cmd/CmdTika.java
+++ src/main/java/bio/guoda/preston/cmd/CmdTika.java
@@ -17,4 +17,10 @@ public class CmdTika extends CmdProcess {
         );
     }
 
+    @Override
+    String getActivityDescription() {
+        return "An event that calculates, if possible, a similarity hash (e.g., hash://tika-tlsh/...) of any encountered content (e.g., hash://sha256/... identifiers). The calculation currently involves generating a similarity hash (e.g., Trend Micro Locality Sensitive Hash, https://github.com/trendmicro/tlsh/blob/master/TLSH_CTC_final.pdf) after extracting the content text using https://tika.apache.org.";
+    }
+
+
 }
