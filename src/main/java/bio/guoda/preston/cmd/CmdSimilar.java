01b27148248636743adfa4b308047c7b29d62a2c
Michael <mielliott@ufl.edu>
2020-07-14 13:35:59 -0400

    make max-hits and similarity-threshold configurable

--- src/main/java/bio/guoda/preston/cmd/CmdSimilar.java
+++ src/main/java/bio/guoda/preston/cmd/CmdSimilar.java
@@ -3,20 +3,32 @@ package bio.guoda.preston.cmd;
 import bio.guoda.preston.process.SimilarContentFinder;
 import bio.guoda.preston.process.StatementsListener;
 import bio.guoda.preston.store.BlobStore;
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.beust.jcommander.converters.FloatConverter;
+import com.beust.jcommander.converters.IntegerConverter;
+import com.beust.jcommander.converters.URIConverter;
 
 import java.io.File;
+import java.net.URI;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Parameters(separators = "= ", commandDescription = "Describes similarity between contents identified by hash://sha256 IRIs according to their hash://tika-tlsh IRIs")
 public class CmdSimilar extends CmdProcess {
+
+    @Parameter(names = {"--hits", "--max-hits", "--matches", "--max-matches"}, description = "maximum number of similar contents to record for each content", converter = IntegerConverter.class)
+    private int maxHits = 100;
+
+    @Parameter(names = {"--thresh", "--threshold", "--similarity-threshold"}, description = "minimum similarity score required for a content similarity to be recorded", converter = FloatConverter.class)
+    private float similarityThreshold = 100f;
 
     private String localIndexDir = getLocalTmpDir() + File.pathSeparator + "index";
 
     @Override
     protected Stream<StatementsListener> createProcessors(BlobStore blobStore, StatementsListener queueAsListener) {
         return Stream.of(
-                new SimilarContentFinder(blobStore, queueAsListener, getDataDir(localIndexDir))
+                new SimilarContentFinder(blobStore, queueAsListener, getDataDir(localIndexDir), maxHits, similarityThreshold)
         );
     }
 
