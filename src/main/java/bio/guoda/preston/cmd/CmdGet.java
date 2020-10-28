dcb3f44f81172512c7f53ef09bbab53550bfe364
Michael <mielliott@ufl.edu>
2020-10-28 13:17:01 -0400

    prepare for more types of queries

--- src/main/java/bio/guoda/preston/cmd/CmdGet.java
+++ src/main/java/bio/guoda/preston/cmd/CmdGet.java
@@ -1,12 +1,13 @@
 package bio.guoda.preston.cmd;
 
-import bio.guoda.preston.model.RefNodeFactory;
 import bio.guoda.preston.process.BlobStoreReadOnly;
 import bio.guoda.preston.store.BlobStoreAppendOnly;
 import bio.guoda.preston.store.KeyValueStoreLocalFileSystem;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.rdf.api.IRI;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,8 +15,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
+import static bio.guoda.preston.model.RefNodeFactory.toIRI;
 import static java.lang.System.exit;
 import static org.apache.commons.io.IOUtils.EOF;
 
@@ -38,11 +44,11 @@ public class CmdGet extends Persisting implements Runnable {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    handleHash(blobStore, StringUtils.trim(line));
+                    handleContentIri(blobStore, StringUtils.trim(line));
                 }
             } else {
                 for (String s : contentUris) {
-                    handleHash(blobStore, s);
+                    handleContentIri(blobStore, s);
                 }
             }
         } catch (Throwable th) {
@@ -51,18 +57,26 @@ public class CmdGet extends Persisting implements Runnable {
         }
     }
 
-    protected void handleHash(BlobStoreReadOnly blobStore, String hash) throws IOException {
+    protected void handleContentIri(BlobStoreReadOnly blobStore, String queryString) throws IOException {
+        ContentQuery query = new ContentQuery(queryString);
+        IRI contentHash = query.getContentIri();
+
         try {
-            InputStream input = blobStore.get(RefNodeFactory.toIRI(hash));
+            InputStream input = blobStore.get(contentHash);
             if (input == null) {
-                System.err.print("not found: [" + hash + "]\n");
+                System.err.print("not found: [" + contentHash.getIRIString() + "]\n");
                 exit(1);
             }
-            copyIfNoError(input, System.out);
+            processQuery(query, input);
 
         } catch (IOException e) {
-            throw new IOException("problem retrieving [" + hash + "]", e);
+            throw new IOException("problem retrieving [" + contentHash.getIRIString() + "]", e);
         }
+    }
+
+    private void processQuery(ContentQuery query, InputStream is) throws IOException {
+        InputStream copyStream = query.dereference(is);
+        copyIfNoError(copyStream, System.out);
     }
 
     protected void copyIfNoError(InputStream proxyIs, PrintStream out) throws IOException {
@@ -75,6 +89,37 @@ public class CmdGet extends Persisting implements Runnable {
 
     public void setContentUris(List<String> contentUris) {
         this.contentUris = contentUris;
+    }
+
+    private class ContentQuery {
+        final Pattern matchOuterQuery = Pattern.compile("^(?<operator>[a-zA-Z0-9]+):(?<inner>\\S+)!\\\\/(?<qualifiers>\\S*)$");
+        LinkedList<Pair<String, String>> queryParts = new LinkedList<>();
+        private final IRI contentIri;
+
+        public ContentQuery(String query) {
+            Matcher matcher = matchOuterQuery.matcher(query);
+
+            while (matcher.find()) {
+                queryParts.addFirst(Pair.of(matcher.group("operator"), matcher.group("qualifiers")));
+            }
+
+            if (queryParts.size() > 0) {
+                contentIri = toIRI(matcher.group("inner"));
+            }
+            else {
+                contentIri = toIRI(query);
+            }
+        }
+
+        public IRI getContentIri() { return contentIri; }
+
+        public Stream<Pair<String, String>> getQueryStream() {
+            return queryParts.stream();
+        }
+
+        public InputStream dereference(InputStream is) {
+            return is;
+        }
     }
 
 }
