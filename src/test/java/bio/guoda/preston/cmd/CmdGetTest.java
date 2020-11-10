b6f4f43fcbf306c6c61c1000be1f51900784527f
Michael <mielliott@ufl.edu>
2020-11-10 12:38:38 -0500

    move CmdGet tests to ContentDereferencerTest

--- src/test/java/bio/guoda/preston/cmd/CmdGetTest.java
+++ src/test/java/bio/guoda/preston/cmd/CmdGetTest.java
@@ -1,6 +1,5 @@
 package bio.guoda.preston.cmd;
 
-import bio.guoda.preston.process.BlobStoreReadOnly;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.rdf.api.IRI;
 import org.junit.Test;
@@ -11,10 +10,8 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.Collections;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static bio.guoda.preston.model.RefNodeFactory.toIRI;
-import static bio.guoda.preston.store.TestUtil.getTestBlobStoreForResource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -43,67 +40,5 @@ public class CmdGetTest {
 
         assertThat(blobStoreNull.getAttemptCount.get(), is(1));
         assertThat(out.toString(), is("some bits and bytes"));
-    }
-
-    @Test
-    public void getByteRange() {
-        BlobStoreReadOnly blobStore = key -> IOUtils.toInputStream("some bits and bytes", Charset.defaultCharset());
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        CmdGet cmdGet = new CmdGet();
-        cmdGet.setContentUris(Collections.singletonList("cut:" + aContentHash + "!/b6-9"));
-        cmdGet.run(blobStore);
-
-        assertThat(out.toString(), is("bits"));
-    }
-
-    @Test
-    public void getArchive() {
-        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
-
-        final AtomicInteger bytesWritten = new AtomicInteger(0);
-        PrintStream out = new PrintStream(new ByteArrayOutputStream()) {
-            @Override
-            public void write(byte[] buf, int off, int len) {
-                bytesWritten.addAndGet(len);
-            }
-        };
-        System.setOut(new PrintStream(out));
-
-        CmdGet cmdGet = new CmdGet();
-        cmdGet.setContentUris(Collections.singletonList(aContentHash));
-        cmdGet.run(blobStore);
-
-        assertThat(bytesWritten.get(), is(303));
-    }
-
-    @Test
-    public void getFileInArchive() {
-        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        CmdGet cmdGet = new CmdGet();
-        cmdGet.setContentUris(Collections.singletonList("tar:gz:" + aContentHash + "!/level1.txt"));
-        cmdGet.run(blobStore);
-
-        assertThat(out.toString(), is("https://example.org"));
-    }
-
-    @Test
-    public void getBytesFromFileInArchive() {
-        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        CmdGet cmdGet = new CmdGet();
-        cmdGet.setContentUris(Collections.singletonList("cut:tar:gz:" + aContentHash + "!/level1.txt!/b9-15"));
-        cmdGet.run(blobStore);
-
-        assertThat(out.toString(), is("example"));
     }
 }
---
+++ src/test/java/bio/guoda/preston/store/ContentDereferencerTest.java
@@ -0,0 +1,61 @@
+package bio.guoda.preston.store;
+
+import bio.guoda.preston.process.BlobStoreReadOnly;
+import org.apache.commons.io.IOUtils;
+import org.apache.james.mime4j.Charsets;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static bio.guoda.preston.model.RefNodeFactory.toIRI;
+import static bio.guoda.preston.store.TestUtil.getTestBlobStoreForResource;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ContentDereferencerTest {
+
+    String aContentHash = "hash://sha256/babababababababababababababababababababababababababababababababa";
+
+    @Test
+    public void getByteRange() throws IOException {
+        BlobStoreReadOnly blobStore = key -> IOUtils.toInputStream("some bits and bytes", Charset.defaultCharset());
+        InputStream content = new ContentDereferencer(blobStore).dereference(toIRI("cut:" + aContentHash + "!/b6-9"));
+        assertThat(IOUtils.toString(content, Charsets.DEFAULT_CHARSET), is("bits"));
+    }
+
+    @Test
+    public void getFileInArchive() throws IOException {
+        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
+        InputStream content = new ContentDereferencer(blobStore).dereference(toIRI("tar:gz:" + aContentHash + "!/level1.txt"));
+        assertThat(IOUtils.toString(content, Charsets.DEFAULT_CHARSET), is("https://example.org"));
+    }
+
+    @Test
+    public void getBytesFromFileInArchive() throws IOException {
+        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
+        InputStream content = new ContentDereferencer(blobStore).dereference(toIRI("cut:tar:gz:" + aContentHash + "!/level1.txt!/b9-15"));
+        assertThat(IOUtils.toString(content, Charsets.DEFAULT_CHARSET), is("example"));
+    }
+
+    @Test
+    public void getArchive() throws IOException {
+        BlobStoreReadOnly blobStore = getTestBlobStoreForResource("/bio/guoda/preston/process/nested.tar.gz");
+
+        final AtomicInteger bytesWritten = new AtomicInteger(0);
+        OutputStream byteGobbler = new OutputStream() {
+            @Override
+            public void write(int b) {
+                bytesWritten.incrementAndGet();
+            }
+        };
+
+        InputStream content = new ContentDereferencer(blobStore).dereference(toIRI(aContentHash));
+        IOUtils.copyLarge(content, byteGobbler);
+
+        assertThat(bytesWritten.get(), is(303));
+    }
+}
