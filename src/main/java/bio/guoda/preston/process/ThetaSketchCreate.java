1c72058b4b10677e2a3b8a2cfce6b7faddfc37f6
jhpoelen <jhpoelen@xs4all.nl>
2021-04-08 17:00:55 -0700

    add alternate implementation for bloomfilter overlap estimation; related to https://github.com/bio-guoda/preston/issues/113;

---
+++ src/main/java/bio/guoda/preston/process/ThetaSketchCreate.java
@@ -0,0 +1,122 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.RDFUtil;
+import bio.guoda.preston.model.RefNodeFactory;
+import bio.guoda.preston.store.BlobStore;
+import bio.guoda.preston.stream.ContentStreamUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Triple;
+import org.apache.commons.rdf.api.BlankNodeOrIRI;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+import org.apache.commons.rdf.simple.Types;
+import org.apache.datasketches.theta.UpdateSketch;
+
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+import static bio.guoda.preston.RefNodeConstants.CONFIDENCE_INTERVAL_95;
+import static bio.guoda.preston.RefNodeConstants.HAS_VALUE;
+import static bio.guoda.preston.RefNodeConstants.THETA_SKETCH_PREFIX;
+import static bio.guoda.preston.RefNodeConstants.WAS_DERIVED_FROM;
+import static bio.guoda.preston.model.RefNodeFactory.toLiteral;
+import static bio.guoda.preston.model.RefNodeFactory.toStatement;
+
+/**
+ * Creates ThetaSketch filter from encountered content values.
+ */
+
+public class ThetaSketchCreate extends StatementProcessor implements Closeable {
+
+
+    private final AtomicReference<Triple<String, UpdateSketch, Quad>> activeFilterContext = new AtomicReference<>();
+
+    private final BlobStore blobStore;
+
+    public ThetaSketchCreate(BlobStore blobStore, StatementsListener listener) {
+        super(listener);
+        this.blobStore = blobStore;
+    }
+
+    @Override
+    public void on(Quad statement) {
+        if (HAS_VALUE.equals(statement.getPredicate())
+                && statement.getSubject() instanceof IRI) {
+
+            IRI contentId = ContentStreamUtil.extractContentHash((IRI) statement.getSubject());
+
+            getActiveFilter(statement, contentId)
+                    .update(RDFUtil.getValueFor(statement.getObject()));
+        }
+    }
+
+    private UpdateSketch getActiveFilter(Quad statement, IRI contentId) {
+        Triple<String, UpdateSketch, Quad> activeFilterContext = this.activeFilterContext.get();
+
+        UpdateSketch filter;
+        if (activeFilterContext == null) {
+            filter = setNewActiveFilter(contentId, statement);
+        } else if (StringUtils.equals(activeFilterContext.getLeft(), contentId.getIRIString())) {
+            filter = activeFilterContext.getMiddle();
+        } else {
+            dereferenceSketch(activeFilterContext);
+            filter = setNewActiveFilter(contentId, statement);
+        }
+        return filter;
+    }
+
+    private void dereferenceSketch(Triple<String, UpdateSketch, Quad> filterPair) {
+        dereferenceFilter(
+                filterPair.getRight(),
+                RefNodeFactory.toIRI(filterPair.getLeft()),
+                filterPair.getMiddle());
+    }
+
+    private UpdateSketch setNewActiveFilter(IRI contentId, Quad statement) {
+        UpdateSketch filter = UpdateSketch.builder().build();
+
+        Triple<String, UpdateSketch, Quad> filterContext
+                = Triple.of(contentId.getIRIString(), filter, statement);
+        activeFilterContext.set(filterContext);
+        return filter;
+    }
+
+    private void dereferenceFilter(Quad statement, IRI contentId, UpdateSketch filter) {
+        try {
+            IRI sketchContentId = blobStore.put(new ByteArrayInputStream(filter.compact().toByteArray()));
+            emitBloomFilter(
+                    filter,
+                    RefNodeFactory.toIRI(THETA_SKETCH_PREFIX + sketchContentId.getIRIString()),
+                    contentId,
+                    statement.getGraphName());
+        } catch (IOException e) {
+            throw new RuntimeException("failed to serialize bloom filter [bloom:gz:" + contentId.getIRIString() + "]", e);
+        }
+    }
+
+    private void emitBloomFilter(UpdateSketch filter,
+                                 IRI sketchHash,
+                                 IRI contentId,
+                                 Optional<BlankNodeOrIRI> parentActivity) {
+
+        ActivityUtil.emitAsNewActivity(
+                Stream.of(toStatement(sketchHash, WAS_DERIVED_FROM, contentId),
+                        toStatement(sketchHash, HAS_VALUE, toLiteral(String.format("%.2f", filter.getEstimate()), Types.XSD_DOUBLE)),
+                        toStatement(sketchHash, CONFIDENCE_INTERVAL_95, toLiteral(String.format("%.2f", filter.getLowerBound(2)), Types.XSD_DOUBLE))),
+                this,
+                parentActivity
+        );
+    }
+
+    @Override
+    public void close() throws IOException {
+        Triple<String, UpdateSketch, Quad> filterContext = activeFilterContext.getAndSet(null);
+        if (filterContext != null) {
+            dereferenceSketch(filterContext);
+        }
+    }
+}
---
+++ src/main/java/bio/guoda/preston/process/ThetaSketchDiff.java
@@ -0,0 +1,160 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.HashType;
+import bio.guoda.preston.model.RefNodeFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.rdf.api.BlankNodeOrIRI;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Literal;
+import org.apache.commons.rdf.api.Quad;
+import org.apache.commons.rdf.api.RDFTerm;
+import org.apache.commons.rdf.simple.Types;
+import org.apache.datasketches.memory.Memory;
+import org.apache.datasketches.theta.CompactSketch;
+import org.apache.datasketches.theta.Intersection;
+import org.apache.datasketches.theta.SetOperation;
+import org.apache.datasketches.theta.Sketch;
+import org.apache.datasketches.theta.Sketches;
+import org.apache.tika.io.IOUtils;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static bio.guoda.preston.RefNodeConstants.CONFIDENCE_INTERVAL_95;
+import static bio.guoda.preston.RefNodeConstants.HAS_VALUE;
+import static bio.guoda.preston.RefNodeConstants.OVERLAPS;
+import static bio.guoda.preston.RefNodeConstants.QUALIFIED_GENERATION;
+import static bio.guoda.preston.RefNodeConstants.THETA_SKETCH_PREFIX;
+import static bio.guoda.preston.RefNodeConstants.USED;
+import static bio.guoda.preston.RefNodeConstants.WAS_DERIVED_FROM;
+import static bio.guoda.preston.model.RefNodeFactory.toIRI;
+import static bio.guoda.preston.model.RefNodeFactory.toLiteral;
+import static bio.guoda.preston.model.RefNodeFactory.toStatement;
+
+/**
+ * Reports on the approximate overlap of dataset elements via encountered Theta Sketches.
+ *
+ * see https://datasketches.apache.org/docs/Theta
+ */
+
+public class ThetaSketchDiff extends ProcessorReadOnly implements Closeable {
+
+    private Map<String, String> encounteredThetaSketches = new TreeMap<>();
+
+    public ThetaSketchDiff(BlobStoreReadOnly blobStoreReadOnly, StatementsListener listener) {
+        super(blobStoreReadOnly, listener);
+    }
+
+    @Override
+    public void on(Quad statement) {
+        if (WAS_DERIVED_FROM.equals(statement.getPredicate())
+                && isOfHashType(statement.getSubject(), THETA_SKETCH_PREFIX)
+                && isOfHashType(statement.getObject(), HashType.sha256.getPrefix())) {
+
+            IRI sketchHash = (IRI) statement.getSubject();
+            IRI shaHash = (IRI) statement.getObject();
+
+            String iriString = shaHash.getIRIString();
+            encounteredThetaSketches.put(iriString, sketchHash.getIRIString());
+
+            try {
+                Sketch bloomFilterCurrent = readSketch(sketchHash);
+                if (bloomFilterCurrent != null) {
+                    Map<String, Pair<Double, Double>> checkedTargets = new TreeMap<>();
+                    for (String contentKey : encounteredThetaSketches.keySet()) {
+                        if (!StringUtils.equals(iriString, contentKey)) {
+                            IRI bloomGzHashTarget = toIRI(encounteredThetaSketches.get(contentKey));
+
+                            if (!checkedTargets.containsKey(bloomGzHashTarget.getIRIString())) {
+                                checkedTargets.put(
+                                        bloomGzHashTarget.getIRIString(),
+                                        calculateApproximateIntersection(bloomFilterCurrent, bloomGzHashTarget)
+                                );
+                            }
+
+                            emitOverlap(
+                                    Pair.of(sketchHash, shaHash),
+                                    checkedTargets.get(bloomGzHashTarget.getIRIString()),
+                                    Pair.of(bloomGzHashTarget, RefNodeFactory.toIRI(contentKey)),
+                                    statement.getGraphName());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("cannot retrieve referenced bloom filter(s)", e);
+            }
+        }
+    }
+
+    private Pair<Double,Double> calculateApproximateIntersection(Sketch bloomFilterCurrent, IRI bloomGzHashTarget) throws IOException {
+        Sketch bloomFilterExisting = readSketch(bloomGzHashTarget);
+        Intersection intersection = SetOperation.builder().buildIntersection();
+        CompactSketch intersect = intersection.intersect(bloomFilterCurrent, bloomFilterExisting);
+
+        double sizeIntersect = intersect.getEstimate();
+
+        return Pair.of(sizeIntersect, intersect.getLowerBound(2));
+    }
+
+    private boolean isOfHashType(RDFTerm object, String prefix) {
+        return object instanceof IRI
+                && ((IRI) object).getIRIString().startsWith(prefix);
+    }
+
+    private Sketch readSketch(IRI contentHash) throws IOException {
+        Sketch sketch = null;
+        String sketchContentId = StringUtils.removeStart(contentHash.getIRIString(), THETA_SKETCH_PREFIX);
+        if (StringUtils.isNotBlank(sketchContentId)) {
+            try (InputStream inputStream = get(toIRI(sketchContentId))) {
+                if (inputStream == null) {
+                    throw new IOException("failed to retrieve theta sketch [" + contentHash.getIRIString() + "]");
+                }
+                sketch = Sketches.wrapSketch(Memory.wrap(IOUtils.toByteArray(inputStream)));
+            }
+        }
+        return sketch;
+    }
+
+    private void emitOverlap(Pair<IRI, IRI> leftBloomAndContent,
+                             Pair<Double,Double> approximateIntersection,
+                             Pair<IRI, IRI> rightBloomAndContent,
+                             Optional<BlankNodeOrIRI> parentActivity) {
+        IRI similarityId = toIRI(UUID.randomUUID());
+        IRI generationId = toIRI(UUID.randomUUID());
+        Stream<Quad> intersectionStatements = Stream.of(
+                toStatement(similarityId, HAS_VALUE, toDoubleLiteral(approximateIntersection.getLeft())),
+                toStatement(similarityId, CONFIDENCE_INTERVAL_95, toDoubleLiteral(approximateIntersection.getRight())),
+                toStatement(similarityId, QUALIFIED_GENERATION, generationId),
+                toStatement(generationId, USED, leftBloomAndContent.getRight()),
+                toStatement(generationId, USED, leftBloomAndContent.getLeft()),
+                toStatement(generationId, USED, rightBloomAndContent.getRight()),
+                toStatement(generationId, USED, rightBloomAndContent.getLeft()));
+
+
+        Stream<Quad> statements = approximateIntersection.getLeft() > 0
+                ? Stream.concat(Stream.of(toStatement(leftBloomAndContent.getValue(), OVERLAPS, rightBloomAndContent.getValue())), intersectionStatements)
+                : intersectionStatements;
+
+        ActivityUtil.emitAsNewActivity(
+                statements,
+                this,
+                parentActivity
+        );
+    }
+
+    public static Literal toDoubleLiteral(Double right) {
+        return toLiteral(String.format("%.2f", right), Types.XSD_DOUBLE);
+    }
+
+    @Override
+    public void close() throws IOException {
+        encounteredThetaSketches.clear();
+    }
+}
