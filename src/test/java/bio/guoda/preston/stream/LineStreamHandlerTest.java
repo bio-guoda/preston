72d5d679c856657eefff53dbc8461bb227eaacf5
jhpoelen <jhpoelen@xs4all.nl>
2021-06-04 16:16:41 -0700

    add explicit test case related to BOM issue; related to https://github.com/bio-guoda/preston/issues/122

--- src/test/java/bio/guoda/preston/stream/LineStreamHandlerTest.java
+++ src/test/java/bio/guoda/preston/stream/LineStreamHandlerTest.java
@@ -6,6 +6,7 @@ import bio.guoda.preston.process.TextMatcher;
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.Quad;
 import org.apache.tika.io.IOUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -23,6 +24,26 @@ public class LineStreamHandlerTest {
 
     @Test
     public void handle() throws IOException, ContentStreamException {
+        List<String> lines = handle("/bio/guoda/preston/process/bhl_item_no_BOM.txt");
+
+        assertThat(lines.size(), is(11));
+        assertThat(lines.get(0), is("ItemID\tTitleID\tThumbnailPageID\tBarCode\tMARCItemID\tCallNumber\tVolumeInfo\tItemURL\tLocalID\tYear\tInstitutionName\tZQuery\tCreationDate"));
+        assertThat(lines.get(lines.size() - 3), is("935\t64\t36774\tmobot31753002306857\ti11595206\tQK1 .F418\tv.23:no.2 (1840)\thttps://www.biodiversitylibrary.org/item/935 \t\t1840\tMissouri Botanical Garden, Peter H. Raven Library\t\t2006-05-04 00:00"));
+        assertThat(lines.get(lines.size() - 1), is(""));
+    }
+
+    @Ignore("see Byte Order Mark issue at https://github.com/bio-guoda/preston/issues/122")
+    @Test
+    public void handleWithBOM() throws IOException, ContentStreamException {
+        List<String> lines = handle("/bio/guoda/preston/process/bhl_item.txt");
+
+        assertThat(lines.size(), is(11));
+        assertThat(lines.get(0), is("ItemID\tTitleID\tThumbnailPageID\tBarCode\tMARCItemID\tCallNumber\tVolumeInfo\tItemURL\tLocalID\tYear\tInstitutionName\tZQuery\tCreationDate"));
+        assertThat(lines.get(lines.size() - 3), is("935\t64\t36774\tmobot31753002306857\ti11595206\tQK1 .F418\tv.23:no.2 (1840)\thttps://www.biodiversitylibrary.org/item/935 \t\t1840\tMissouri Botanical Garden, Peter H. Raven Library\t\t2006-05-04 00:00"));
+        assertThat(lines.get(lines.size() - 1), is(""));
+    }
+
+    private List<String> handle(String pathToResource) throws ContentStreamException, IOException {
         List<String> lines = new LinkedList<>();
         ContentStreamHandler testHandler = new ContentStreamHandler() {
             @Override
@@ -42,15 +63,12 @@ public class LineStreamHandlerTest {
         };
 
         IRI contentIri = RefNodeFactory.toIRI("blah");
-        BlobStoreReadOnly store = getTestBlobStoreForResource("/bio/guoda/preston/process/bhl_item_no_BOM.txt");
+        BlobStoreReadOnly store = getTestBlobStoreForResource(pathToResource);
 
         LineStreamHandler lineHandler = new LineStreamHandler(testHandler);
         lineHandler.handle(contentIri, store.get(contentIri));
 
-        assertThat(lines.size(), is(11));
-        assertThat(lines.get(0), is("ItemID\tTitleID\tThumbnailPageID\tBarCode\tMARCItemID\tCallNumber\tVolumeInfo\tItemURL\tLocalID\tYear\tInstitutionName\tZQuery\tCreationDate"));
-        assertThat(lines.get(lines.size() - 3), is("935\t64\t36774\tmobot31753002306857\ti11595206\tQK1 .F418\tv.23:no.2 (1840)\thttps://www.biodiversitylibrary.org/item/935 \t\t1840\tMissouri Botanical Garden, Peter H. Raven Library\t\t2006-05-04 00:00"));
-        assertThat(lines.get(lines.size() - 1), is(""));
+        return lines;
     }
 
     @Test
