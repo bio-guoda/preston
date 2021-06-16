540971734e2b757228f55467f434fd6ef50dd6c2
jhpoelen <jhpoelen@xs4all.nl>
2021-06-16 15:58:03 -0700

    towards introducing a separate preston content module; fyi @mielliott; related to https://github.com/bio-guoda/preston/issues/127

--- .travis/version_if_tag.sh
+++ .travis/version_if_tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ -n "$TRAVIS_TAG" ]; then
   VERSION=${TRAVIS_TAG//[^0-9.]/}
+  mvn versions:set -DnewVersion=$VERSION -am -pl preston-cli
   mvn versions:set -DnewVersion=$VERSION
 fi
