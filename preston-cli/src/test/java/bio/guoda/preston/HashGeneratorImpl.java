140ff0e6ea4875576a5d828bcf098a89d330ffef
jhpoelen <jhpoelen@xs4all.nl>
2022-03-22 17:11:56 -0500

    more refactoring

---
+++ preston-cli/src/test/java/bio/guoda/preston/HashGeneratorImpl.java
@@ -0,0 +1,25 @@
+package bio.guoda.preston;
+
+import org.apache.commons.rdf.api.IRI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class HashGeneratorImpl extends HashGeneratorAbstract<IRI> {
+
+    private final HashType type;
+
+    public HashGeneratorImpl() {
+        this(HashType.sha256);
+    }
+
+    public HashGeneratorImpl(HashType type) {
+        this.type = type;
+    }
+
+    @Override
+    public IRI hash(InputStream is, OutputStream os, boolean shouldCloseInputStream) throws IOException {
+        return Hasher.calcHashIRI(is, os, shouldCloseInputStream, type);
+    }
+}
--- preston-cli/src/test/java/bio/guoda/preston/HashGeneratorImplTest.java
+++ preston-cli/src/test/java/bio/guoda/preston/HashGeneratorImplTest.java
@@ -12,25 +12,33 @@ import java.nio.charset.StandardCharsets;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class HashGeneratorSHA256Test {
+public class HashGeneratorImplTest {
 
     @Test
     public void hashTextFromResource() throws IOException {
         String name = "/bio/guoda/preston/process/idigbio-recordsets-complete.json";
         InputStream is = TestUtil.filterLineFeedFromTextInputStream(getClass().getResourceAsStream(name));
-        IRI hash = new HashGeneratorSHA256().hash(is);
+        IRI hash = new HashGeneratorImpl().hash(is);
         assertThat(hash.getIRIString(), is("hash://sha256/0931a831be557bfedd27b0068d9a0a9f14c1b92cbf9199e8ba79e04d0a6baedc"));
     }
 
     @Test
+    public void hashTextFromResourceMD5() throws IOException {
+        String name = "/bio/guoda/preston/process/idigbio-recordsets-complete.json";
+        InputStream is = TestUtil.filterLineFeedFromTextInputStream(getClass().getResourceAsStream(name));
+        IRI hash = new HashGeneratorImpl(HashType.md5).hash(is);
+        assertThat(hash.getIRIString(), is("hash://md5/b5c57e251f776bb71b4c41fac5aee270"));
+    }
+
+    @Test
     public void hashTextFromString() throws IOException {
-        IRI hash = new HashGeneratorSHA256().hash(IOUtils.toInputStream("hello", StandardCharsets.UTF_8));
+        IRI hash = new HashGeneratorImpl().hash(IOUtils.toInputStream("hello", StandardCharsets.UTF_8));
         assertThat(hash.getIRIString(), is("hash://sha256/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"));
     };
 
     @Test
     public void hashBinary() throws IOException {
-        IRI hash = new HashGeneratorSHA256().hash(getClass().getResourceAsStream("/bio/guoda/preston/dwca-20180905.zip"));
+        IRI hash = new HashGeneratorImpl().hash(getClass().getResourceAsStream("/bio/guoda/preston/dwca-20180905.zip"));
         assertThat(hash.getIRIString(), is("hash://sha256/59f32445a50646d923f8ba462a7d87a848632f28bd93ac579de210e3375714de"));
     };
 
