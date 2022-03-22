a6adfc9002eb8bb21b9938b349e00812d097a4c9
jhpoelen <jhpoelen@xs4all.nl>
2022-03-22 17:15:11 -0500

    more refactoring

--- preston-core/src/main/java/bio/guoda/preston/store/BlobStoreAppendOnly.java
+++ preston-core/src/main/java/bio/guoda/preston/store/BlobStoreAppendOnly.java
@@ -12,22 +12,27 @@ public class BlobStoreAppendOnly implements BlobStore {
 
     private final KeyValueStore keyValueStore;
     private final boolean shouldCloseInputStream;
-    private final HashType types = HashType.sha256;
+    private final HashType type;
 
     public BlobStoreAppendOnly(KeyValueStore keyValueStore) {
         this(keyValueStore, true);
     }
 
     public BlobStoreAppendOnly(KeyValueStore keyValueStore, boolean shouldCloseInputStream) {
+        this(keyValueStore, shouldCloseInputStream, HashType.sha256);
+    }
+
+    public BlobStoreAppendOnly(KeyValueStore keyValueStore, boolean shouldCloseInputStream, HashType type) {
         this.keyValueStore = keyValueStore;
         this.shouldCloseInputStream = shouldCloseInputStream;
+        this.type = type;
     }
 
     // write-once, read-many
     @Override
     public IRI put(InputStream is) throws IOException {
         return keyValueStore.put(
-                (is1, os1) -> Hasher.calcHashIRI(is1, os1, shouldCloseInputStream, types), is);
+                (is1, os1) -> Hasher.calcHashIRI(is1, os1, shouldCloseInputStream, type), is);
     }
 
     @Override
