aac9f61062f42eb58cbaa5ca9f058f023f4e6f35
jhpoelen <jhpoelen@xs4all.nl>
2022-04-14 12:03:55 -0500

    remove unused

--- preston-cli/src/main/java/bio/guoda/preston/cmd/SketchTypeConverter.java
+++
@@ -1,9 +0,0 @@
-package bio.guoda.preston.cmd;
-
-import com.beust.jcommander.converters.EnumConverter;
-
-public class SketchTypeConverter extends EnumConverter<SketchType> {
-    public SketchTypeConverter(String optionName, Class<SketchType> clazz) {
-        super(optionName, clazz);
-    }
-}
