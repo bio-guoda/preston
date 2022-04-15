3f5ab14b6949b81c2982de84f9eb9ddd86bb5dbf
jhpoelen <jhpoelen@xs4all.nl>
2022-04-15 16:25:49 -0500

    remove unused class

--- preston-cli/src/main/java/bio/guoda/preston/cmd/HashPathPatternConverter.java
+++
@@ -1,9 +0,0 @@
-package bio.guoda.preston.cmd;
-
-import com.beust.jcommander.converters.EnumConverter;
-
-public class HashPathPatternConverter extends EnumConverter<ArchiveType> {
-    public HashPathPatternConverter(String optionName, Class<ArchiveType> clazz) {
-        super(optionName, clazz);
-    }
-}
