1f0d6fdabb33dcf77e5fc2f4d23c7c07ec351328
jhpoelen <jhpoelen@xs4all.nl>
2022-07-20 14:07:35 -0500

    add integration tests for dataone retrieval; related to https://github.com/bio-guoda/preston/issues/181

--- preston-cli/src/test/java/bio/guoda/preston/cmd/PrestonIT.java
+++ preston-cli/src/test/java/bio/guoda/preston/cmd/PrestonIT.java
@@ -7,7 +7,23 @@ public class PrestonIT {
 
     @Test
     public void history() {
-        Preston.run(new String[]{"history","--remote","https://deeplinker.bio/"});
+        Preston.run(new String[]{
+                "history",
+                "--remote",
+                "https://deeplinker.bio/"
+        });
+    }
+
+    @Test
+    public void catDataOne() {
+        Preston.run(new String[]{
+                "cat",
+                "--algo",
+                "md5",
+                "--remote",
+                "https://dataone.org",
+                "hash://md5/e27c99a7f701dab97b7d09c467acf468"
+        });
     }
 
 }
