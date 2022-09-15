06aa4449189c030d3debc28a8f3e3d8c8138c0cb
jhpoelen <jhpoelen@xs4all.nl>
2022-09-15 12:52:39 -0500

    add [qrcode] and [head] commands to help cite a most recent archive version visually with a qrcode; and textually by printing the associated hash uri; related to https://github.com/bio-guoda/preston/issues/189

---
+++ preston-label/src/main/java/bio/guoda/preston/cmd/CmdGenerateQRCode.java
@@ -0,0 +1,49 @@
+package bio.guoda.preston.cmd;
+
+import bio.guoda.preston.store.VersionUtil;
+import com.google.zxing.WriterException;
+import org.apache.commons.rdf.api.IRI;
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+
+@CommandLine.Command(
+        name = "qrcode",
+        aliases = {"label"},
+        description = "generates a printable PNG image QRCode of most recent provenance log version. See also \"head\"."
+)
+public class CmdGenerateQRCode extends LoggingPersisting implements Runnable {
+
+    @Override
+    public void run() {
+        AtomicReference<IRI> mostRecentLog = new AtomicReference<>();
+        try {
+            getTracerOfDescendants()
+                    .trace(
+                            getProvenanceRoot(),
+                            statement -> {
+                                IRI iri = VersionUtil.mostRecentVersionForStatement(statement);
+                                if (iri != null) {
+                                    mostRecentLog.set(iri);
+                                }
+                            }
+                    );
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to get version history.", e);
+        }
+
+        if (mostRecentLog.get() == null) {
+            throw new RuntimeException("Cannot generate QRCode: no provenance logs found.");
+        }
+
+        try {
+            QRCodeGenerator.generateQRCode(mostRecentLog.get(), getOutputStream());
+        } catch (IOException | WriterException e) {
+            throw new RuntimeException("failed to generate QRCode for [" + mostRecentLog.get().getIRIString() + "]", e);
+        }
+
+    }
+
+}
+
