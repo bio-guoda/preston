5c20a43d4bb5251a413bce358f33badb3081c283
mielliott <mielliott@ufl.edu>
2022-11-03 13:28:30 -0400

    Add RDF reader for building a Lucene index

---
+++ preston-registry/src/main/java/bio/guoda/preston/process/RDFReader.java
@@ -0,0 +1,61 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.RefNodeConstants;
+import bio.guoda.preston.RefNodeFactory;
+import bio.guoda.preston.store.BlobStore;
+import bio.guoda.preston.store.StatementProcessor;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+public class RDFReader extends StatementProcessor {
+
+    private final IRI qualifiedGeneration;
+    private final BlobStore blobStore;
+    private final StatementsEmitter quadProcessor;
+
+    public RDFReader(BlobStore blobStore, StatementsListener listener, IRI qualifiedGeneration, StatementsEmitter quadProcessor) {
+        super(listener);
+        this.qualifiedGeneration = qualifiedGeneration;
+        this.blobStore = blobStore;
+        this.quadProcessor = quadProcessor;
+    }
+
+    @Override
+    public void on(Quad statement) {
+        getLatestVersion(statement).ifPresent(this::getAndProcessRDF);
+    }
+
+    private void getAndProcessRDF(IRI version) {
+        emit(RefNodeFactory.toStatement(
+                qualifiedGeneration,
+                qualifiedGeneration,
+                RefNodeConstants.USED,
+                version
+        ));
+
+        try {
+            processRDF(blobStore.get(version));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to dereference " + version, e);
+        }
+    }
+
+    private void processRDF(InputStream inputStream) {
+        new EmittingStreamRDF(quadProcessor).parseAndEmit(inputStream);
+    }
+
+    private Optional<IRI> getLatestVersion(Quad statement) {
+        if (statement.getPredicate().equals(RefNodeConstants.HAS_VERSION)) {
+            return Optional.of((IRI) statement.getObject());
+        } else if (statement.getPredicate().equals(RefNodeConstants.HAS_PREVIOUS_VERSION)) {
+            return Optional.of((IRI) statement.getSubject());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+}
---
+++ preston-registry/src/test/java/bio/guoda/preston/process/RDFReaderTest.java
@@ -0,0 +1,67 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.HashType;
+import bio.guoda.preston.Hasher;
+import bio.guoda.preston.RefNodeConstants;
+import bio.guoda.preston.store.BlobStore;
+import bio.guoda.preston.store.TestUtilForProcessor;
+import org.apache.commons.io.input.NullInputStream;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+import static bio.guoda.preston.RefNodeFactory.toIRI;
+import static bio.guoda.preston.RefNodeFactory.toStatement;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+
+public class RDFReaderTest {
+
+    @Test
+    public void recordProvVersions() {
+        Stream<Quad> quads = Stream.of(
+                toStatement(toIRI("bloop"), RefNodeConstants.HAS_VERSION, toIRI("hash://sha256/bloop")),
+                toStatement(toIRI("hash://sha256/blip"), RefNodeConstants.HAS_PREVIOUS_VERSION, toIRI("hash://sha256/bloop")),
+                toStatement(toIRI("blup"), RefNodeConstants.USED, toIRI("hash://sha256/blip"))
+        );
+
+        LinkedList<Quad> nodes = new LinkedList<>();
+
+        IRI qualifiedGeneration = toIRI("00000000-0000-0000-0000-000000000000");
+        RDFReader indexer = new RDFReader(
+                new TestBlobStore(),
+                TestUtilForProcessor.testListener(nodes),
+                qualifiedGeneration,
+                new StatementsEmitterAdapter() {
+                    @Override
+                    public void emit(Quad statement) {
+                    }
+                }
+        );
+
+        quads.forEach(indexer::on);
+
+        assertThat(nodes.size(), is(2));
+        assertThat(nodes.get(0).toString(), equalTo("<00000000-0000-0000-0000-000000000000> <http://www.w3.org/ns/prov#used> <hash://sha256/bloop> <00000000-0000-0000-0000-000000000000> ."));
+        assertThat(nodes.get(1).toString(), equalTo("<00000000-0000-0000-0000-000000000000> <http://www.w3.org/ns/prov#used> <hash://sha256/blip> <00000000-0000-0000-0000-000000000000> ."));
+    }
+
+    private static class TestBlobStore implements BlobStore {
+        @Override
+        public IRI put(InputStream is) throws IOException {
+            return Hasher.calcHashIRI(is, NullOutputStream.NULL_OUTPUT_STREAM, HashType.sha256);
+        }
+
+        @Override
+        public InputStream get(IRI key) {
+            return new NullInputStream();
+        }
+    }
+}
