6fdde9d6f28440f4ad784359792e2ce9af34b1e0
mielliott <mielliott@ufl.edu>
2022-11-11 17:26:47 -0500

    Test unpacking a lucene index

--- preston-search/src/test/java/bio/guoda/preston/ArchiveUtilTest.java
+++ preston-search/src/test/java/bio/guoda/preston/ArchiveUtilTest.java
@@ -8,25 +8,29 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ArchiveUtilTest {
 
-    private static String NESTED_TAR_GZ = "/bio/guoda/preston/process/nested.tar.gz";
+    private static String INDEX_TAR_GZ = "/bio/guoda/preston/index/tiny-lucene-index.tar.gz";
 
     @Test
     public void copyDirectoryToBlobstoreAsTarGz() {
 
     }
 
+    @SuppressWarnings("DataFlowIssue")
     @Test
     public void unpackTarGzFromBlobstore() throws IOException {
-        BlobStoreReadOnly blobStore = TestUtil.getTestBlobStoreForResource(NESTED_TAR_GZ);
+        BlobStoreReadOnly blobStore = TestUtil.getTestBlobStoreForResource(INDEX_TAR_GZ);
         File destination = FileUtils.getTempDirectory().toPath().resolve(UUID.randomUUID().toString()).toFile();
 
         ArchiveUtil.unpackTarGzFromBlobstore(
@@ -36,8 +40,20 @@ public class ArchiveUtilTest {
         );
 
         assertTrue(destination.isDirectory());
-        File[] firstLevelFiles = Objects.requireNonNull(destination.listFiles());
-        assertThat(firstLevelFiles.length, is(37));
+
+        File unpackedDirectory = destination.toPath().resolve("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").toFile();
+        assertTrue(unpackedDirectory.isDirectory());
+
+        Set<String> contents = Arrays.stream(unpackedDirectory.listFiles())
+                .map(File::getName)
+                .collect(Collectors.toSet());
+
+        assertThat(contents.size(), is(5));
+        assertThat(contents, hasItem("_0.cfe"));
+        assertThat(contents, hasItem("_0.cfs"));
+        assertThat(contents, hasItem("_0.si"));
+        assertThat(contents, hasItem("segments_2"));
+        assertThat(contents, hasItem("write.lock"));
     }
 
     private static BlobStoreAppendOnly createTestBlobStore() {
