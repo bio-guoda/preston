c8e8c7e26b0cbd151517c6465f008411d9c1c036
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-01-30 11:37:52 -0600

    reproduced root cause of stopped processing https://github.com/bio-guoda/preston/issues/214

--- preston-get/src/test/java/bio/guoda/preston/process/EmittingStreamRDFTest.java
+++ preston-get/src/test/java/bio/guoda/preston/process/EmittingStreamRDFTest.java
@@ -96,6 +96,39 @@ public class EmittingStreamRDFTest {
     }
 
     @Test
+    public void emitQuadsSkipMalformed() {
+        // for some reason line 114 of hash://sha256/18b51a180c63929d5e3a50dbb72295579c2645546d22ae3fdcd5e2095c43d199
+        // contains malformed rdf quads
+        // also see https://github.com/bio-guoda/preston/issues/214
+        // E.g.,
+        // preston cat --remote https://linker.bio 'line:hash://sha256/18b51a180c63929d5e3a50dbb72295579c2645546d22ae3fdcd5e2095c43d199!/L114'
+        //
+        // produces:
+        //
+        // 001/XMLSchema#dateTime> <urn:uuid:8ae298b0-bf2a-4ce4-aa44-b41b0a0d0f6a> .
+        //
+        List<Quad> quads = new ArrayList<>();
+        new EmittingStreamRDF(new StatementsEmitterAdapter() {
+            @Override
+            public void emit(Quad statement) {
+                quads.add(statement);
+            }
+        }, new ProcessorState() {
+            @Override
+            public boolean shouldKeepProcessing() {
+                return true;
+            }
+
+            @Override
+            public void stopProcessing() {
+
+            }
+        }).parseAndEmit(getClass().getResourceAsStream("/bio/guoda/preston/store/issue-214-data-example/18/b5/18b51a180c63929d5e3a50dbb72295579c2645546d22ae3fdcd5e2095c43d199"));
+
+        assertThat(quads.size(), Is.is(1));
+    }
+
+    @Test
     public void emitQuadNeverStart() {
         List<Quad> quads = new ArrayList<>();
         new EmittingStreamRDF(new StatementsEmitterAdapter() {
