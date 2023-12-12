2f2a0de19da4c952a6115d5674daee8a57d756d0
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-12-12 16:20:56 -0600

    remove unused imports

--- preston-cas/src/main/java/bio/guoda/preston/stream/ContentStreamHandler.java
+++ preston-cas/src/main/java/bio/guoda/preston/stream/ContentStreamHandler.java
@@ -1,12 +1,9 @@
 package bio.guoda.preston.stream;
 
-import bio.guoda.preston.process.ProcessorState;
 import bio.guoda.preston.process.ProcessorStateReadOnly;
 import org.apache.commons.rdf.api.IRI;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 
 public interface ContentStreamHandler extends ProcessorStateReadOnly {
 
