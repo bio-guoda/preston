428e9a4e354b39f9d5f96571ebac655cafdf212e
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-12-21 17:22:47 -0600

    add provenance info into redirect http headers; related to https://github.com/bio-guoda/preston/issues/199

--- preston-serve/src/main/java/bio/guoda/preston/server/RedirectingServlet.java
+++ preston-serve/src/main/java/bio/guoda/preston/server/RedirectingServlet.java
@@ -1,6 +1,7 @@
 package bio.guoda.preston.server;
 
 import bio.guoda.preston.HashType;
+import bio.guoda.preston.MimeTypes;
 import bio.guoda.preston.RefNodeFactory;
 import bio.guoda.preston.ResourcesHTTP;
 import bio.guoda.preston.store.HashKeyUtil;
@@ -22,6 +23,8 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -32,6 +35,17 @@ public class RedirectingServlet extends HttpServlet {
 
     public static final Pattern URN_UUID_REQUEST_PATTERN
             = Pattern.compile("^urn:uuid:" + UUIDUtil.UUID_PATTERN_PART + "$");
+    public static final String SEEN_AT = "seenAt";
+    public static final String PROVENANCE_ID = "provenanceId";
+    public static final String ARCHIVE_URL = "archiveUrl";
+    public static final String UUID = "uuid";
+    public static final String DOI = "doi";
+    public static final String CONTENT_ID = "contentId";
+    public static final String ACTIVITY = "activity";
+    public static final String QUERY_TYPE_UUID = "uuid";
+    public static final String QUERY_TYPE_HASH = "hash";
+    public static final String QUERY_TYPE_URL = "url";
+    public static final String QUERY_TYPE_DOI = "doi";
 
     @Override
     public void destroy() {
@@ -51,7 +65,7 @@ public class RedirectingServlet extends HttpServlet {
 
         String queryType = queryTypeForRequestedId(requestedId);
 
-        if (StringUtils.equals(queryType, "doi")) {
+        if (StringUtils.equals(queryType, QUERY_TYPE_DOI)) {
             requestedId = "https://doi.org/" + requestedId;
         }
 
@@ -61,29 +75,42 @@ public class RedirectingServlet extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } else {
             log("attempting to direct [" + requestedId + "] as [" + queryType + "]");
-            try {
-                String contentId = findMostRecentContentId(
-                        requestedIdIRI,
-                        queryType,
-                        sparqlEndpoint);
-                if (StringUtils.isNotBlank(contentId)) {
-                    URI uri = HashKeyUtil.insertSlashIfNeeded(URI.create(resolverEndpoint),  contentId);
-                    response.setHeader(HttpHeaders.LOCATION, uri.toString());
-                    response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-                    log("response [" + requestedIdIRI.getIRIString() + "]");
-                } else {
-                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                }
-            } catch (Throwable th) {
-                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                log("not found [" + requestedIdIRI.getIRIString() + "]");
-                try {
-                    throw th;
-                } catch (URISyntaxException e) {
-                    throw new ServletException(e);
-                }
-            }
+            handleRequest(response, resolverEndpoint, sparqlEndpoint, queryType, requestedIdIRI);
 
+        }
+    }
+
+    protected void handleRequest(HttpServletResponse response, String resolverEndpoint, String sparqlEndpoint, String queryType, IRI requestedIdIRI) throws IOException, ServletException {
+        try {
+            Map<String, String> provInfo = findMostRecentContentId(
+                    requestedIdIRI,
+                    queryType,
+                    sparqlEndpoint);
+            if (provInfo.containsKey(CONTENT_ID) && StringUtils.isNotBlank(provInfo.get(CONTENT_ID))) {
+                String contentId = provInfo.get(CONTENT_ID);
+                URI uri = HashKeyUtil.insertSlashIfNeeded(URI.create(resolverEndpoint), contentId);
+                response.setHeader(HttpHeaders.LOCATION, uri.toString());
+                response.setHeader(HttpHeaders.CONTENT_TYPE, MimeTypes.MIME_TYPE_DWCA);
+                response.setHeader(HttpHeaders.ETAG, contentId);
+                response.setHeader(HttpHeaders.CONTENT_LOCATION, provInfo.get(ARCHIVE_URL));
+                response.setHeader(HttpHeaders.DATE, provInfo.get(SEEN_AT));
+                response.setHeader("X-UUID", provInfo.get(UUID));
+                response.setHeader("X-DOI", provInfo.get(DOI));
+                response.setHeader("X-PROVENANCE-ANCHOR", provInfo.get(PROVENANCE_ID));
+                response.setHeader("X-PROVENANCE-ACTIVITY", provInfo.get(ACTIVITY));
+                response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+                log("response [" + requestedIdIRI.getIRIString() + "]");
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (Throwable th) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            log("not found [" + requestedIdIRI.getIRIString() + "]");
+            try {
+                throw th;
+            } catch (URISyntaxException e) {
+                throw new ServletException(e);
+            }
         }
     }
 
@@ -99,24 +126,27 @@ public class RedirectingServlet extends HttpServlet {
 
     static String queryTypeForRequestedId(String requestURI) {
         return Stream.of(requestURI)
-                .map(req -> URN_UUID_REQUEST_PATTERN.matcher(req).matches() ? "uuid" : req)
-                .map(req -> HashType.sha1.getIRIPattern().matcher(req).matches() ? "hash" : req)
-                .map(req -> HashType.md5.getIRIPattern().matcher(req).matches() ? "hash" : req)
-                .map(req -> HashType.sha256.getIRIPattern().matcher(req).matches() ? "hash" : req)
-                .map(req -> Pattern.compile("^(10[.])([^/]+)/(.*)$").matcher(req).matches() ? "doi" : req)
-                .map(req -> Pattern.compile("^http[s]{0,1}://[^ ]+").matcher(req).matches() ? "url" : req)
+                .map(req -> URN_UUID_REQUEST_PATTERN.matcher(req).matches() ? QUERY_TYPE_UUID : req)
+                .map(req -> HashType.sha1.getIRIPattern().matcher(req).matches() ? QUERY_TYPE_HASH : req)
+                .map(req -> HashType.md5.getIRIPattern().matcher(req).matches() ? QUERY_TYPE_HASH : req)
+                .map(req -> HashType.sha256.getIRIPattern().matcher(req).matches() ? QUERY_TYPE_HASH : req)
+                .map(req -> Pattern.compile("^(10[.])([^/]+)/(.*)$").matcher(req).matches() ? QUERY_TYPE_DOI : req)
+                .map(req -> Pattern.compile("^http[s]{0,1}://[^ ]+").matcher(req).matches() ? QUERY_TYPE_URL : req)
                 .findFirst()
                 .orElse("unknown");
     }
 
-    public static String findMostRecentContentId(IRI iri1, String paramName, String sparqlEndpoint) throws IOException, URISyntaxException {
-        String iri = iri1.toString();
+    static Map<String, String> findMostRecentContentId(IRI iri, String paramName, String sparqlEndpoint) throws IOException, URISyntaxException {
+        String response = findProvenance(iri, paramName, sparqlEndpoint);
+        return extractProvenanceInfo(response);
+    }
 
+    protected static String findProvenance(IRI iri, String paramName, String sparqlEndpoint) throws IOException, URISyntaxException {
         InputStream resourceAsStream = RedirectingServlet.class.getResourceAsStream(paramName + ".rq");
 
         String queryTemplate = IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
 
-        String queryString = StringUtils.replace(queryTemplate, "?_" + paramName + "_iri", iri);
+        String queryString = StringUtils.replace(queryTemplate, "?_" + paramName + "_iri", iri.toString());
 
         URI query = new URI("https", "example.org", "/query", "query=" + queryString, null);
 
@@ -125,33 +155,27 @@ public class RedirectingServlet extends HttpServlet {
         IRI dataURI = RefNodeFactory.toIRI(endpoint);
 
         InputStream inputStream = ResourcesHTTP.asInputStream(dataURI);
-        String response = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-
-        return extractFirstContentId(response);
+        return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
     }
 
-    private static String extractFirstContentId(String response) throws JsonProcessingException {
-        return extractFirstContentId(new ObjectMapper().readTree(response));
+    private static Map<String, String> extractProvenanceInfo(String response) throws JsonProcessingException {
+        return extractProvenanceInfo(new ObjectMapper().readTree(response));
     }
 
-    static String extractFirstContentId(JsonNode jsonNode) {
-        String contentId = null;
+    static Map<String, String> extractProvenanceInfo(JsonNode jsonNode) {
+        Map<String, String> attributes = new TreeMap<String, String>();
         if (jsonNode.has("results")) {
             JsonNode result = jsonNode.get("results");
             if (result.has("bindings")) {
                 for (JsonNode binding : result.get("bindings")) {
-                    if (binding.has("contentId")) {
-                        JsonNode jsonNode1 = binding.get("contentId");
-                        if (jsonNode1.has("value")) {
-                            contentId = jsonNode1.get("value").asText();
-                            break;
-                        }
-                    }
+                    binding.fieldNames()
+                            .forEachRemaining(key -> attributes.put(key, binding.get(key).has("value") ? binding.get(key).get("value").asText() : ""));
+                    break;
                 }
             }
 
         }
-        return contentId;
+        return attributes;
     }
 
 
--- preston-serve/src/test/java/bio/guoda/preston/server/RedirectingServletTest.java
+++ preston-serve/src/test/java/bio/guoda/preston/server/RedirectingServletTest.java
@@ -8,7 +8,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Map;
 
+import static bio.guoda.preston.server.RedirectingServlet.ACTIVITY;
+import static bio.guoda.preston.server.RedirectingServlet.CONTENT_ID;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RedirectingServletTest {
@@ -16,71 +19,78 @@ public class RedirectingServletTest {
 
     @Test
     public void dealiasDOI() throws IOException, URISyntaxException {
-        String contentId = RedirectingServlet.findMostRecentContentId(
+        Map<String, String> contentId = RedirectingServlet.findMostRecentContentId(
                 RefNodeFactory.toIRI("https://doi.org/10.15468/aomfnb"),
-                "doi", "https://lod.globalbioticinteractions.org/query"
+                RedirectingServlet.QUERY_TYPE_DOI, "https://lod.globalbioticinteractions.org/query"
         );
-        assertThat(contentId, Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(contentId.get(CONTENT_ID), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(contentId.get(ACTIVITY), Is.is("urn:uuid:77f3faf7-acd2-4f14-9c0e-4e04ef5b63c7"));
     }
 
     @Test
     public void dealiasHash() throws IOException, URISyntaxException {
-        String contentId = RedirectingServlet.findMostRecentContentId(
+        Map<String, String> contentId = RedirectingServlet.findMostRecentContentId(
                 RefNodeFactory.toIRI("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"),
-                "hash",
+                RedirectingServlet.QUERY_TYPE_HASH,
                 "https://lod.globalbioticinteractions.org/query");
-        assertThat(contentId, Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(contentId.get(CONTENT_ID), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
 
     }
 
     @Test
     public void dealiasUUID() throws IOException, URISyntaxException {
-        String contentId = RedirectingServlet.findMostRecentContentId(
+        Map<String, String> contentId = RedirectingServlet.findMostRecentContentId(
                 RefNodeFactory.toIRI("urn:uuid:4fa7b334-ce0d-4e88-aaae-2e0c138d049e"),
-                "uuid",
+                RedirectingServlet.QUERY_TYPE_UUID,
                 "https://lod.globalbioticinteractions.org/query");
-        assertThat(contentId, Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(contentId.get(CONTENT_ID), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
 
     }
 
     @Test
     public void dealiasURL() throws URISyntaxException, IOException {
-        String contentId = RedirectingServlet.findMostRecentContentId(
+        Map<String, String> contentId = RedirectingServlet.findMostRecentContentId(
                 RefNodeFactory.toIRI("https://hosted-datasets.gbif.org/eBird/2022-eBird-dwca-1.0.zip"),
-                "url",
+                RedirectingServlet.QUERY_TYPE_URL,
                 "https://lod.globalbioticinteractions.org/query");
-        assertThat(contentId, Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(contentId.get(CONTENT_ID), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
     }
 
     @Test
     public void parseResult() throws IOException {
         JsonNode response = new ObjectMapper().readTree(getClass().getResourceAsStream("url-response.json"));
-        assertThat(RedirectingServlet.extractFirstContentId(response), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        Map<String, String> actual = RedirectingServlet.extractProvenanceInfo(response);
+        assertThat(actual.get(CONTENT_ID), Is.is("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d"));
+        assertThat(actual.get(RedirectingServlet.DOI), Is.is("https://doi.org/10.15468/aomfnb"));
+        assertThat(actual.get(RedirectingServlet.UUID), Is.is("urn:uuid:4fa7b334-ce0d-4e88-aaae-2e0c138d049e"));
+        assertThat(actual.get(RedirectingServlet.ARCHIVE_URL), Is.is("https://hosted-datasets.gbif.org/eBird/2022-eBird-dwca-1.0.zip"));
+        assertThat(actual.get(RedirectingServlet.SEEN_AT), Is.is("2023-12-02T16:05:25.261Z"));
+        assertThat(actual.get(RedirectingServlet.PROVENANCE_ID), Is.is("hash://sha256/5b7fa37bf8b64e7c935c4ff3389e36f8dd162f0705410dd719fd089e1ea253cd"));
 
     }
 
     @Test
     public void queryTypeDOI() {
         String s = RedirectingServlet.queryTypeForRequestedId("10.123/2345");
-        assertThat(s, Is.is("doi"));
+        assertThat(s, Is.is(RedirectingServlet.QUERY_TYPE_DOI));
     }
 
     @Test
     public void queryTypeUUID() {
         String s = RedirectingServlet.queryTypeForRequestedId("urn:uuid:4fa7b334-ce0d-4e88-aaae-2e0c138d049e");
-        assertThat(s, Is.is("uuid"));
+        assertThat(s, Is.is(RedirectingServlet.QUERY_TYPE_UUID));
     }
 
     @Test
     public void queryTypeHash() {
         String s = RedirectingServlet.queryTypeForRequestedId("hash://sha256/1e2b7436fce1848f41698e5a9c193f311abaf0ee051bec1a2e48b5106d29524d");
-        assertThat(s, Is.is("hash"));
+        assertThat(s, Is.is(RedirectingServlet.QUERY_TYPE_HASH));
     }
 
     @Test
     public void queryTypeUrl() {
         String s = RedirectingServlet.queryTypeForRequestedId("https://example.org");
-        assertThat(s, Is.is("url"));
+        assertThat(s, Is.is(RedirectingServlet.QUERY_TYPE_URL));
     }
 
 }
