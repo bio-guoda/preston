559cbdbddaffb05a14f7db29f3d9792f732eb66c
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-01-25 12:07:21 -0600

    towards implementing TaxoDros streaming support; related to https://github.com/bio-guoda/preston/issues/275

---
+++ preston-taxodros/src/main/java/bio/guoda/preston/cmd/TaxoDrosFileStreamHandler.java
@@ -0,0 +1,141 @@
+package bio.guoda.preston.cmd;
+
+import bio.guoda.preston.store.Dereferencer;
+import bio.guoda.preston.stream.ContentStreamException;
+import bio.guoda.preston.stream.ContentStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.txt.UniversalEncodingDetector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class TaxoDrosFileStreamHandler implements ContentStreamHandler {
+
+    private static final String PREFIX_AUTHOR = ".A ";
+    private static final String PREFIX_TITLE = ".S ";
+    private static final String PREFIX_YEAR = ".J ";
+    private static final String PREFIX_METHOD_DIGITIZATION = ".K ";
+    private static final String PREFIX_JOURNAL = ".Z ";
+    private static final String PREFIX_FILENAME = ".P ";
+
+    private final Dereferencer<InputStream> dereferencer;
+    private ContentStreamHandler contentStreamHandler;
+    private final OutputStream outputStream;
+
+    public TaxoDrosFileStreamHandler(ContentStreamHandler contentStreamHandler,
+                                     Dereferencer<InputStream> inputStreamDereferencer,
+                                     OutputStream os) {
+        this.contentStreamHandler = contentStreamHandler;
+        this.dereferencer = inputStreamDereferencer;
+        this.outputStream = os;
+    }
+
+    @Override
+    public boolean handle(IRI version, InputStream is) throws ContentStreamException {
+        AtomicBoolean foundAtLeastOne = new AtomicBoolean(false);
+        String iriString = version.getIRIString();
+        try {
+            Charset charset = new UniversalEncodingDetector().detect(is, new Metadata());
+            if (charset != null) {
+                int lineStart = -1;
+                int lineFinish = -1;
+                AtomicReference<StringBuilder> textCapture = new AtomicReference<>(new StringBuilder());
+                ObjectNode objectNode = new ObjectMapper().createObjectNode();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, charset));
+
+                for (int lineNumber = 1; contentStreamHandler.shouldKeepProcessing(); ++lineNumber) {
+                    String line = reader.readLine();
+                    if (line == null || StringUtils.startsWith(line, ".TEXT;")) {
+                        lineFinish = lineNumber - 1;
+                        if (lineFinish > lineStart && objectNode.size() > 0) {
+                            setValue(objectNode, "filename", getAndResetCapture(textCapture));
+                            setValue(objectNode, "http://www.w3.org/ns/prov#wasDerivedFrom", "line:" + iriString + "!/L" + lineStart + "-" + "L" + lineFinish);
+                            setValue(objectNode, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "taxodros-flatfile");
+                            IOUtils.copy(IOUtils.toInputStream(objectNode.toString(), StandardCharsets.UTF_8), outputStream);
+                            IOUtils.copy(IOUtils.toInputStream("\n", StandardCharsets.UTF_8), outputStream);
+                            foundAtLeastOne.set(true);
+                            lineFinish = -1;
+                        }
+                        lineStart = lineNumber;
+                        objectNode.removeAll();
+                        getAndResetCapture(textCapture);
+                        if (line == null) {
+                            break;
+                        }
+                    } else if (StringUtils.startsWith(line, PREFIX_AUTHOR)) {
+                        setValue(objectNode, "id", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_AUTHOR);
+                    } else if (StringUtils.startsWith(line, PREFIX_YEAR)) {
+                        setValue(objectNode, "authors", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_YEAR);
+                    } else if (StringUtils.startsWith(line, PREFIX_TITLE)) {
+                        setValue(objectNode, "year", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_TITLE);
+                    } else if (StringUtils.startsWith(line, PREFIX_JOURNAL)) {
+                        setValue(objectNode, "title", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_JOURNAL);
+                    } else if (StringUtils.startsWith(line, PREFIX_METHOD_DIGITIZATION)) {
+                        setValue(objectNode, "journal", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_METHOD_DIGITIZATION);
+                    } else if (StringUtils.startsWith(line, PREFIX_FILENAME)) {
+                        setValue(objectNode, "method", getAndResetCapture(textCapture));
+                        append(textCapture, line, PREFIX_FILENAME);
+                    } else {
+                        append(textCapture, line);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new ContentStreamException("no charset detected");
+        }
+
+        return foundAtLeastOne.get();
+    }
+
+    private void append(AtomicReference<StringBuilder> textCapture, String line, String prefix) {
+        String value = getValueWithLinePrefix(line, prefix);
+        append(textCapture, value);
+    }
+
+    private StringBuilder append(AtomicReference<StringBuilder> textCapture, String line) {
+        return textCapture.get().append(" ").append(StringUtils.trim(line));
+    }
+
+    private String getAndResetCapture(AtomicReference<StringBuilder> captureBuffer) {
+        StringBuilder captured = captureBuffer.getAndSet(new StringBuilder());
+        return StringUtils.trim(captured.toString());
+    }
+
+
+    private void setValue(ObjectNode objectNode, String key, String value) {
+        objectNode.set(key, TextNode.valueOf(value));
+    }
+
+    private static String getValueWithLinePrefix(String line, CharSequence prefix) {
+        int start = prefix.length();
+        int end = line.length();
+        return StringUtils.trim(
+                StringUtils.substring(line, start, end));
+    }
+
+    @Override
+    public boolean shouldKeepProcessing() {
+        return contentStreamHandler.shouldKeepProcessing();
+    }
+
+
+}
---
+++ preston-taxodros/src/test/java/bio/guoda/preston/cmd/TaxoDrosFileExtractorTest.java
@@ -0,0 +1,90 @@
+package bio.guoda.preston.cmd;
+
+import bio.guoda.preston.process.ProcessorStateAlwaysContinue;
+import bio.guoda.preston.store.BlobStoreReadOnly;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static bio.guoda.preston.RefNodeConstants.HAS_VERSION;
+import static bio.guoda.preston.RefNodeFactory.toIRI;
+import static bio.guoda.preston.RefNodeFactory.toStatement;
+import static junit.framework.TestCase.assertNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.Is.is;
+
+public class TaxoDrosFileExtractorTest {
+
+    @Test
+    public void streamTaxoDrosToLineJson() throws IOException {
+
+        BlobStoreReadOnly blobStore = new BlobStoreReadOnly() {
+            @Override
+            public InputStream get(IRI key) {
+                URL resource = getClass().getResource("DROS5.TEXT.example.txt");
+                IRI iri = toIRI(resource.toExternalForm());
+
+                if (StringUtils.equals("hash://sha256/856ecd48436bb220a80f0a746f94abd7c4ea47cb61d946286f7e25cf0ec69dc1", key.getIRIString())) {
+                    try {
+                        return new FileInputStream(new File(URI.create(iri.getIRIString())));
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                return null;
+            }
+        };
+
+        Quad statement = toStatement(
+                toIRI("blip"),
+                HAS_VERSION,
+                toIRI("hash://sha256/856ecd48436bb220a80f0a746f94abd7c4ea47cb61d946286f7e25cf0ec69dc1")
+        );
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        TaxoDrosFileExtractor extractor = new TaxoDrosFileExtractor(
+                new ProcessorStateAlwaysContinue(),
+                blobStore,
+                byteArrayOutputStream
+        );
+
+        extractor.on(statement);
+
+        String actual = IOUtils.toString(
+                byteArrayOutputStream.toByteArray(),
+                StandardCharsets.UTF_8.name()
+        );
+
+        String[] jsonObjects = StringUtils.split(actual, "\n");
+        assertThat(jsonObjects.length, is(3));
+
+        JsonNode taxonNode = new ObjectMapper().readTree(jsonObjects[0]);
+
+        assertThat(taxonNode.get("http://www.w3.org/ns/prov#wasDerivedFrom").asText(), is("line:hash://sha256/856ecd48436bb220a80f0a746f94abd7c4ea47cb61d946286f7e25cf0ec69dc1!/L1-L10"));
+        assertThat(taxonNode.get("http://www.w3.org/1999/02/22-rdf-syntax-ns#type").asText(), is("taxodros-flatfile"));
+        assertThat(taxonNode.get("authors").asText(), is("Abd El-Halim, A.S., Mostafa, A.A., & Allam, K.A.M.a.,"));
+        assertThat(taxonNode.get("title").asText(), is("Dipterous flies species and their densities in fourteen Egyptian governorates."));
+        assertThat(taxonNode.get("journal").asText(), is("J. Egypt. Soc. Parasitol., 35:351-362."));
+        assertThat(taxonNode.get("year").asText(), is("2005"));
+        assertThat(taxonNode.get("method").asText(), is("ocr"));
+        assertThat(taxonNode.get("filename").asText(), is("Abd El-Halim et al., 2005M.pdf"));
+
+    }
+
+
+}
