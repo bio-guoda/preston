543eb409cddffa1355be61702b306322e7c7bba6
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-05-07 18:51:56 -0500

    towards supporting google drive resource tracking

---
+++ preston-track/src/main/java/bio/guoda/preston/process/RegistryReaderGoogleDrive.java
@@ -0,0 +1,179 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.RefNodeConstants;
+import bio.guoda.preston.RefNodeFactory;
+import bio.guoda.preston.store.KeyValueStoreReadOnly;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static bio.guoda.preston.RefNodeFactory.hasVersionAvailable;
+import static bio.guoda.preston.RefNodeFactory.toStatement;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.csv;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.docx;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.epub;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.odp;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.ods;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.odt;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.pdf;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.pptx;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.rtf;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.tsv;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.txt;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.xlsx;
+
+public class RegistryReaderGoogleDrive extends ProcessorReadOnly {
+
+    private static final Pattern GOOGLE_DRIVE_URL_PATTERN
+            = Pattern.compile("https://.*google.com/(?<type>[a-z]+)/d/(?<id>[a-zA-Z0-9-_]+)/.*");
+    private static final Pattern GID_PATTERN
+            = Pattern.compile(".*gid=(?<gid>[0-9]+).*");
+    private static final GoogleResourceId NOT_A_GOOGLE_RESOURCE_ID
+            = new GoogleResourceId(null, null, null);
+
+    private static final TreeMap<String, List<Type>> TYPES_FOR_TYPES = new TreeMap<String, List<Type>>() {{
+        put("document", Arrays.asList(pdf, docx, txt, odt, rtf, epub));
+        put("presentation", Arrays.asList(pdf, pptx, odp, txt));
+        put("spreadsheets", Arrays.asList(xlsx, ods, pdf, csv, tsv));
+    }};
+
+    public RegistryReaderGoogleDrive(KeyValueStoreReadOnly blobStoreReadOnly, StatementsListener... listeners) {
+        super(blobStoreReadOnly, listeners);
+    }
+
+    @Override
+    public void on(Quad statement) {
+        if (hasVersionAvailable(statement)) {
+            IRI versionSource = RefNodeFactory.getVersionSource(statement);
+            String versionSourceString = versionSource.getIRIString();
+            GoogleResourceId googleResourceId = getGoogleResourceId(versionSourceString);
+            if (likelyGoogleResource(googleResourceId)) {
+                exportTypesFor(googleResourceId)
+                        .forEach(idType -> emitExportStatements(
+                                idType.getKey(),
+                                idType.getRight(),
+                                versionSourceString,
+                                this)
+                        );
+            }
+        }
+    }
+
+    private Stream<Pair<GoogleResourceId, Type>> exportTypesFor(GoogleResourceId googleResourceId) {
+        List<Type> types = TYPES_FOR_TYPES.get(googleResourceId.getType());
+        return types == null ? Stream.empty() : types.stream().map(t -> Pair.of(googleResourceId, t));
+    }
+
+    private static boolean likelyGoogleResource(GoogleResourceId googleResourceId) {
+        return !NOT_A_GOOGLE_RESOURCE_ID.equals(googleResourceId);
+    }
+
+    enum Type {
+        txt("txt", "text/plain"),
+        csv("csv", "text/csv"),
+        tsv("tsv", "text/tab-separated-values"),
+        pdf("pdf", "application/pdf"),
+        docx("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
+        xlsx("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+        pptx("pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"),
+        epub("epub", "application/epub+zip"),
+        odt("odt", "application/vnd.oasis.opendocument.text"),
+        ods("ods", "application/vnd.oasis.opendocument.spreadsheet"),
+        odp("odp", "application/vnd.oasis.opendocument.presentation"),
+        rtf("rtf", "application/application/rtf");
+
+        public String getMimeType() {
+            return mimeType;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        private final String mimeType;
+        private final String type;
+
+        Type(String type, String mimeType) {
+            this.type = type;
+            this.mimeType = mimeType;
+        }
+    }
+
+    static class GoogleResourceId {
+        private final String type;
+
+        private final String id;
+        private final String gid;
+
+        GoogleResourceId(String id, String type) {
+            this(id, type, null);
+        }
+
+        GoogleResourceId(String id, String type, String gid) {
+            this.id = id;
+            this.type = type;
+            this.gid = gid;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getGid() {
+            return gid;
+        }
+
+    }
+
+    static GoogleResourceId getGoogleResourceId(String url) {
+        GoogleResourceId grid = NOT_A_GOOGLE_RESOURCE_ID;
+        Matcher matcher = GOOGLE_DRIVE_URL_PATTERN.matcher(url);
+        if (matcher.matches()) {
+            grid = new GoogleResourceId(matcher.group("id"), matcher.group("type"));
+            if (StringUtils.equals("spreadsheets", matcher.group("type"))) {
+                Matcher matcher1 = GID_PATTERN.matcher(url);
+                if (matcher1.matches()) {
+                    grid = new GoogleResourceId(matcher.group("id"), matcher.group("type"), matcher1.group("gid"));
+                }
+            }
+        }
+        return grid;
+    }
+
+    static void emitExportStatements(GoogleResourceId id, Type type, String originalUrl, StatementEmitter emitter) {
+        if (likelyGoogleResource(id)) {
+            Stream.of(
+                    toStatement(
+                            toExportIRI(id),
+                            RefNodeConstants.WAS_DERIVED_FROM,
+                            RefNodeFactory.toIRI(originalUrl)),
+                    toStatement(
+                            toExportIRI(id),
+                            RefNodeConstants.HAS_FORMAT,
+                            RefNodeFactory.toLiteral(type.mimeType)),
+                    toStatement(
+                            toExportIRI(id),
+                            RefNodeConstants.HAS_VERSION,
+                            RefNodeFactory.toBlank())
+            ).forEach(emitter::emit);
+        }
+    }
+
+    private static IRI toExportIRI(GoogleResourceId id) {
+        String exportUrlPrefix = "https://docs.google.com/document/u/0/export?id=";
+        return RefNodeFactory.toIRI(exportUrlPrefix + id.getId() + "&format=" + id.getType());
+    }
+
+}
---
+++ preston-track/src/test/java/bio/guoda/preston/process/RegistryReaderGoogleDriveTest.java
@@ -0,0 +1,180 @@
+package bio.guoda.preston.process;
+
+import bio.guoda.preston.RefNodeConstants;
+import bio.guoda.preston.RefNodeFactory;
+import bio.guoda.preston.store.KeyValueStoreReadOnly;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.commons.rdf.api.Quad;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.docx;
+import static bio.guoda.preston.process.RegistryReaderGoogleDrive.Type.pdf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RegistryReaderGoogleDriveTest {
+
+
+    @Test
+    public void handleVersionForGoogleResource() {
+        List<Quad> statements = new ArrayList<>();
+        RegistryReaderGoogleDrive registryReaderGoogleDrive = new RegistryReaderGoogleDrive(new KeyValueStoreReadOnly() {
+            @Override
+            public InputStream get(IRI uri) throws IOException {
+                throw new IOException("kaboom!");
+            }
+        }, new StatementsListenerAdapter() {
+
+            @Override
+            public void on(Quad statement) {
+                statements.add(statement);
+            }
+        });
+
+        registryReaderGoogleDrive.on(
+                RefNodeFactory.toStatement(
+                        RefNodeFactory.toIRI("https://docs.google.com/document/d/1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4/edit#heading=h.s91o7eupvwg7"),
+                        RefNodeConstants.HAS_VERSION,
+                        RefNodeFactory.toIRI("foo:bar"))
+        );
+
+        assertThat(statements.size(), Is.is(18));
+    }
+
+    @Test
+    public void handleVersionForNonGoogleResource() {
+        List<Quad> statements = new ArrayList<>();
+        RegistryReaderGoogleDrive registryReaderGoogleDrive = new RegistryReaderGoogleDrive(new KeyValueStoreReadOnly() {
+            @Override
+            public InputStream get(IRI uri) throws IOException {
+                throw new IOException("kaboom!");
+            }
+        }, new StatementsListenerAdapter() {
+
+            @Override
+            public void on(Quad statement) {
+                statements.add(statement);
+            }
+        });
+
+        registryReaderGoogleDrive.on(
+                RefNodeFactory.toStatement(
+                        RefNodeFactory.toIRI("https://example.org"),
+                        RefNodeConstants.HAS_VERSION,
+                        RefNodeFactory.toIRI("foo:bar"))
+        );
+
+        assertThat(statements.size(), Is.is(0));
+    }
+
+    @Test
+    public void handleVersionForGoogleResourceExport() {
+        List<Quad> statements = new ArrayList<>();
+        RegistryReaderGoogleDrive registryReaderGoogleDrive = new RegistryReaderGoogleDrive(new KeyValueStoreReadOnly() {
+            @Override
+            public InputStream get(IRI uri) throws IOException {
+                throw new IOException("kaboom!");
+            }
+        }, new StatementsListenerAdapter() {
+
+            @Override
+            public void on(Quad statement) {
+                statements.add(statement);
+            }
+        });
+
+        registryReaderGoogleDrive.on(
+                RefNodeFactory.toStatement(
+                        RefNodeFactory.toIRI("https://docs.google.com/document/u/0/export?id=1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4&type=pdf"),
+                        RefNodeConstants.HAS_VERSION,
+                        RefNodeFactory.toIRI("foo:bar"))
+        );
+
+        assertThat(statements.size(), Is.is(0));
+    }
+
+
+    @Test
+    public void detectDocURL() {
+        String url = "https://docs.google.com/document/d/1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4/edit#heading=h.s91o7eupvwg7";
+
+        RegistryReaderGoogleDrive.GoogleResourceId gid = RegistryReaderGoogleDrive.getGoogleResourceId(url);
+
+        assertThat(gid.getId(), Is.is("1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4"));
+        assertThat(gid.getType(), Is.is("document"));
+    }
+
+    @Test
+    public void generateDerivedResourcesStatements() {
+        String url = "https://docs.google.com/document/d/1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4/edit#heading=h.s91o7eupvwg7";
+
+        List<Quad> statements = new ArrayList<>();
+
+        StatementEmitter emitter = new StatementEmitter() {
+
+            @Override
+            public void emit(Quad statement) {
+                statements.add(statement);
+            }
+        };
+
+
+        RegistryReaderGoogleDrive.emitExportStatements(RegistryReaderGoogleDrive.getGoogleResourceId(url), pdf, url, emitter);
+        RegistryReaderGoogleDrive.emitExportStatements(RegistryReaderGoogleDrive.getGoogleResourceId(url), docx, url, emitter);
+
+
+        assertThat(statements.size(), Is.is(6));
+
+    }
+
+    @Test
+    public void generateDerivedResourcesStatements2() {
+        String url = "https://docs.google.com/document/u/0/export?id=1jzb54GbAkB_TOFIjWof5BW6gn1ujSnXXJQu6aZgFAB4&type=pdf";
+
+        List<Quad> statements = new ArrayList<>();
+
+        StatementEmitter emitter = new StatementEmitter() {
+
+            @Override
+            public void emit(Quad statement) {
+                statements.add(statement);
+            }
+        };
+
+        RegistryReaderGoogleDrive.GoogleResourceId googleResourceId = RegistryReaderGoogleDrive.getGoogleResourceId(url);
+
+        RegistryReaderGoogleDrive.emitExportStatements(googleResourceId, pdf, url, emitter);
+
+
+        assertThat(statements.size(), Is.is(0));
+
+    }
+
+
+    @Test
+    public void detectSheetsURL() {
+        String url = "https://docs.google.com/spreadsheets/d/1wFuJ4RRlNirnrPfuY_d57I9_pnaNibw4nltNTkruSp0/edit#gid=1784126572";
+
+        RegistryReaderGoogleDrive.GoogleResourceId googleResourceId = RegistryReaderGoogleDrive.getGoogleResourceId(url);
+
+        assertThat(googleResourceId.getId(), Is.is("1wFuJ4RRlNirnrPfuY_d57I9_pnaNibw4nltNTkruSp0"));
+        assertThat(googleResourceId.getType(), Is.is("spreadsheets"));
+        assertThat(googleResourceId.getGid(), Is.is("1784126572"));
+
+    }
+
+    @Test
+    public void detectPresentationSheetsURL() {
+        String url = "https://docs.google.com/presentation/d/1kV0tVscrYO6WxZYRupnzjVQecmmky27Oc7YeePZjBXg/edit#slide=id.p";
+
+        RegistryReaderGoogleDrive.GoogleResourceId googleResourceId = RegistryReaderGoogleDrive.getGoogleResourceId(url);
+        assertThat(googleResourceId.getId(), Is.is("1kV0tVscrYO6WxZYRupnzjVQecmmky27Oc7YeePZjBXg"));
+        assertThat(googleResourceId.getType(), Is.is("presentation"));
+    }
+
+}
