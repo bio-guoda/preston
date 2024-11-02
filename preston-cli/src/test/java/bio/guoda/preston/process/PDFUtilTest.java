d547d7b6c5e532fb799f0fb03fecb5d67f72df5f
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-11-01 19:33:53 -0500

    towards supporting non-numeric pdf page references; https://github.com/bio-guoda/preston/issues/308

--- preston-cli/src/test/java/bio/guoda/preston/process/PDFUtilTest.java
+++ preston-cli/src/test/java/bio/guoda/preston/process/PDFUtilTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -86,11 +87,57 @@ public class PDFUtilTest {
     }
 
     @Test
+    public void selectPageWithNonNumericLabel() throws IOException {
+        String pageLabel = "IV";
+        int expectedPageCount = 232;
+
+        // Also see BatLit - Krumbach, T., Koopman, K. F., & Krumbach, T. (1994). Chiroptera, Teilbd. 60: systematics. de Gruyter. https://doi.org/10.5281/zenodo.13422270 https://linker.bio/hash://md5/70c3e0fba7379e09e95a38569fe29da7
+
+        String resourceName = "koopman1994.pdf";
+        IRI baseContentId = Hasher
+                .calcHashIRI(getClass().getResourceAsStream(resourceName), NullOutputStream.INSTANCE, HashType.md5);
+
+
+        IRI contentId = toIRI("pdf:" + baseContentId.getIRIString() + "!/p" + pageLabel);
+
+        PDDocument pdDocument = Loader.loadPDF(new File(getClass().getResource(resourceName).getFile()));
+
+        assertThat(pdDocument.getNumberOfPages(), is(expectedPageCount));
+
+        PDDocumentCatalog documentCatalog = pdDocument.getDocumentCatalog();
+
+        Integer integer = documentCatalog.getPageLabels().getPageIndicesByLabels().get(pageLabel);
+
+        assertThat(integer, is(3));
+
+        PDPage page = pdDocument.getPage(3);
+
+        PDPageLabelRange pageLabelRange = documentCatalog.getPageLabels().getPageLabelRange(3);
+
+        ByteArrayOutputStream output1 = new ByteArrayOutputStream();
+
+        PDFUtil.saveAsPDF(new PageSelected(page, 0, pageLabelRange), contentId, output1);
+
+        PDDocument actualDoc = Loader.loadPDF(output1.toByteArray());
+        String customMetadataValue = actualDoc.getDocumentInformation().getCustomMetadataValue(RefNodeConstants.WAS_DERIVED_FROM.getIRIString());
+
+        InputStream resourceAsStream = getClass().getResourceAsStream("koopman1994-pageIV.pdf");
+        ByteArrayOutputStream output2 = new ByteArrayOutputStream();
+        IOUtils.copy(resourceAsStream, output2);
+
+        assertThat(customMetadataValue, is(contentId.getIRIString()));
+
+
+        assertThat(output1.toString(), is(output2.toString()));
+
+
+    }
+
+
+    @Test
     public void selectPageWithoutLabel() throws IOException, ContentStreamException {
         String requestedPageLabel = "3";
         int expectedPageCount = 13;
-
-        // Also see BatLit - Krumbach, T., Koopman, K. F., & Krumbach, T. (1994). Chiroptera, Teilbd. 60: systematics. de Gruyter. https://doi.org/10.5281/zenodo.13422270 https://linker.bio/hash://md5/70c3e0fba7379e09e95a38569fe29da7
 
         String resourceName = "elliott2023.pdf";
         IRI baseContentId = Hasher
