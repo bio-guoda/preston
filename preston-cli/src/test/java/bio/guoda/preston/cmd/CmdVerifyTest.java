751601e3b1c9790d8d22c8ba103139e7ab6f236f
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-01-13 17:33:14 -0600

    add test data

--- preston-cli/src/test/java/bio/guoda/preston/cmd/CmdVerifyTest.java
+++ preston-cli/src/test/java/bio/guoda/preston/cmd/CmdVerifyTest.java
@@ -18,7 +18,27 @@ public class CmdVerifyTest {
     @Test
     public void verifySha256() throws URISyntaxException {
 
-        URL resource = getClass().getResource("/bio/guoda/preston/cmd/verify-data/data/2a/5d/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a");
+        ByteArrayOutputStream stdout = configAndRun("/bio/guoda/preston/cmd/verify-data/2a/5d/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a");
+
+        String actual = new String(stdout.toByteArray(), StandardCharsets.UTF_8);
+        assertThat(actual, Is.is(not("")));
+        assertThat(actual, containsString("f0c131ebf6ad2dce71ab9a10aa116dcedb219ae4539f9e5bf0e57b84f51f22ca\tFAIL\tMISSING\t"));
+        assertThat(actual, containsString("854831f2665681b90818b902104aa1212a1f1e38a2307a7d66ca1dfad4d8240c\tOK\tCONTENT_PRESENT_VALID_HASH"));
+    }
+
+    @Test
+    public void verifySha256InnerHash() throws URISyntaxException {
+
+        ByteArrayOutputStream stdout = configAndRun("/bio/guoda/preston/cmd/verify-data/2a/5d/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a");
+
+        String actual = new String(stdout.toByteArray(), StandardCharsets.UTF_8);
+        assertThat(actual, Is.is(not("")));
+        assertThat(actual, containsString("f0c131ebf6ad2dce71ab9a10aa116dcedb219ae4539f9e5bf0e57b84f51f22ca\tFAIL\tMISSING\t"));
+        assertThat(actual, not(containsString("line:hash://sha256/854831f2665681track818b902104aa1212a1f1e38a2307a7d66ca1dfad4d8240c!/L1")));
+    }
+
+    private ByteArrayOutputStream configAndRun(String resourcePath) throws URISyntaxException {
+        URL resource = getClass().getResource(resourcePath);
 
         File dataDir = new File(resource.toURI()).getParentFile().getParentFile().getParentFile();
 
@@ -27,11 +47,7 @@ public class CmdVerifyTest {
         cmdVerify.setOutputStream(stdout);
         cmdVerify.setDataDir(dataDir.getAbsolutePath());
         cmdVerify.run();
-
-        String actual = new String(stdout.toByteArray(), StandardCharsets.UTF_8);
-        assertThat(actual, Is.is(not("")));
-        assertThat(actual, containsString("f0c131ebf6ad2dce71ab9a10aa116dcedb219ae4539f9e5bf0e57b84f51f22ca\tFAIL\tMISSING\t"));
-        assertThat(actual, containsString("854831f2665681b90818b902104aa1212a1f1e38a2307a7d66ca1dfad4d8240c\tOK\tCONTENT_PRESENT_VALID_HASH"));
+        return stdout;
     }
 
 }
