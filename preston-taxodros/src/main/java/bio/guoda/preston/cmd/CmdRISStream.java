ad01c19225013091c29c7eda5e6a75b12c9ad4e6
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-08 18:40:58 -0500

    update ris-stream documentation

--- preston-taxodros/src/main/java/bio/guoda/preston/cmd/CmdRISStream.java
+++ preston-taxodros/src/main/java/bio/guoda/preston/cmd/CmdRISStream.java
@@ -21,7 +21,13 @@ import java.util.List;
         footerHeading = "Examples",
         footer = {
                 "%n1.",
-                "First, generate a RIS record, record.ris:",
+                "First, append the associated bhl pdf via: ",
+                "----",
+                "preston track https://www.biodiversitylibrary.org/partpdf/326364",
+                "----",
+                "Finally, generate Zenodo metadata record.json using: ",
+                "----",
+                "Following, generate a RIS record, record.ris:",
                 "----",
                 "cat > record.ris <<__EOL__",
                 "TY  - BOOK",
@@ -38,16 +44,66 @@ import java.util.List;
                 "cat record.ris\\",
                 " | preston track",
                 "----",
-                "Following, append the associated bhl pdf via: ",
-                "----",
-                "preston track https://www.biodiversitylibrary.org/partpdf/326364",
-                "----",
-                "Finally, generate Zenodo metadata record.json using: ",
-                "----",
                 "preston head\\",
                 " | preston cat\\",
                 " | preston ris-stream\\",
                 " > record.json",
+                "----",
+                "where record.json:",
+                "----",
+                "{\n" +
+                        "  \"metadata\": {\n" +
+                        "    \"description\": \"(Uploaded by Plazi from the Biodiversity Heritage Library) No abstract provided.\",\n" +
+                        "    \"communities\": [],\n" +
+                        "    \"http://www.w3.org/ns/prov#wasDerivedFrom\": \"https://linker.bio/line:hash://sha256/5fd5944b52b22efc56f901d96ff53a64c42e1f2264763e2f1074ac2c589e47cf!/L1-L7\",\n" +
+                        "    \"http://www.w3.org/1999/02/22-rdf-syntax-ns#type\": \"application/x-research-info-systems\",\n" +
+                        "    \"title\": \"Faber, Helen R May 5, 1913\",\n" +
+                        "    \"upload_type\": \"publication\",\n" +
+                        "    \"publication_type\": \"other\",\n" +
+                        "    \"journal_title\": \"Walter Deane correspondence\",\n" +
+                        "    \"publication_date\": \"1913-05-05\",\n" +
+                        "    \"referenceId\": \"https://www.biodiversitylibrary.org/part/326364\",\n" +
+                        "    \"filename\": \"bhlpart326364.pdf\",\n" +
+                        "    \"keywords\": [\n" +
+                        "      \"Biodiversity\",\n" +
+                        "      \"BHL-Corpus\",\n" +
+                        "      \"Source: Biodiversity Heritage Library\",\n" +
+                        "      \"Source: https://biodiversitylibrary.org\",\n" +
+                        "      \"Source: BHL\"\n" +
+                        "    ],\n" +
+                        "    \"creators\": [\n" +
+                        "      {\n" +
+                        "        \"name\": \"Faber, Helen R.\"\n" +
+                        "      }\n" +
+                        "    ],\n" +
+                        "    \"related_identifiers\": [\n" +
+                        "      {\n" +
+                        "        \"relation\": \"isDerivedFrom\",\n" +
+                        "        \"identifier\": \"https://linker.bio/line:hash://sha256/5fd5944b52b22efc56f901d96ff53a64c42e1f2264763e2f1074ac2c589e47cf!/L1-L7\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"relation\": \"isDerivedFrom\",\n" +
+                        "        \"identifier\": \"https://www.biodiversitylibrary.org/part/326364\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"relation\": \"isAlternateIdentifier\",\n" +
+                        "        \"identifier\": \"urn:lsid:biodiversitylibrary.org:part:326364\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"relation\": \"isPartOf\",\n" +
+                        "        \"identifier\": \"hash://sha256/3983c9abbba981838de5d47a5dadf94c4afcea7df63486effb71d780e592ebe8\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"relation\": \"hasVersion\",\n" +
+                        "        \"identifier\": \"hash://md5/7fddbf186c6bbddb0b49919fc340bb61\"\n" +
+                        "      },\n" +
+                        "      {\n" +
+                        "        \"relation\": \"hasVersion\",\n" +
+                        "        \"identifier\": \"hash://sha256/9b30af8f432b78e0d739b0457376dac998057a5b4b5fccd52b81560ec1f4f146\"\n" +
+                        "      }\n" +
+                        "    ]\n" +
+                        "  }\n" +
+                        "}\n",
                 "----"
         }
 
