b0210ed3c827a30794af79de0ea9383532b8655d
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-17 12:58:40 -0600

    remove unused import

--- preston-get/src/test/java/bio/guoda/preston/cmd/CmdGetIT.java
+++ preston-get/src/test/java/bio/guoda/preston/cmd/CmdGetIT.java
@@ -20,7 +20,6 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.fail;
 
 public class CmdGetIT {
