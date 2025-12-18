47375d19e11b5c32a8c8388236773dc67142fecb
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-17 19:02:27 -0600

    remove unused travis config

--- .travis/version_if_tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -n "$TRAVIS_TAG" ]; then
-  VERSION=${TRAVIS_TAG//[^0-9.]/}
-  mvn versions:set -DnewVersion=$VERSION -am -pl preston-cli
-  mvn versions:set -DnewVersion=$VERSION
-fi
